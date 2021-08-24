e1ea68b0bb65f20c964a2af7ff7e95d1c293feda
Michal Ziulek <michalziulek@gmail.com>
2021-08-24 23:14:41 +0200

    imgui_test: Progress.

--- samples/imgui_test/src/imgui_test.zig
+++ samples/imgui_test/src/imgui_test.zig
@@ -18,6 +18,8 @@ const DemoState = struct {
     grfx: gr.GraphicsContext,
     gui: gr.GuiContext,
     frame_stats: lib.FrameStats,
+
+    //ui: struct {},
 };
 
 fn init(allocator: *std.mem.Allocator) DemoState {
@@ -50,7 +52,56 @@ fn update(demo: *DemoState) void {
 
     lib.updateWindow(demo.frame_stats.delta_time);
 
-    c.igShowDemoWindow(null);
+    const main_viewport = c.igGetMainViewport();
+    c.igSetNextWindowPos(
+        c.ImVec2{ .x = main_viewport.*.WorkPos.x + 650.0, .y = main_viewport.*.WorkPos.y + 20.0 },
+        c.ImGuiCond_FirstUseEver,
+        c.ImVec2{ .x = 0.0, .y = 0.0 },
+    );
+    c.igSetNextWindowSize(c.ImVec2{ .x = 550.0, .y = 680.0 }, c.ImGuiCond_FirstUseEver);
+
+    if (!c.igBegin("Gui Demo", null, c.ImGuiWindowFlags_None)) {
+        c.igEnd();
+        return;
+    }
+
+    const static = struct {
+        var number: u32 = 0;
+    };
+
+    c.igPushItemWidth(-120.0);
+
+    c.igText("dear imgui says hello. (%s)", c.igGetVersion());
+    c.igSpacing();
+
+    if (c.igCollapsingHeader_TreeNodeFlags("Help", c.ImGuiTreeNodeFlags_None)) {
+        c.igText("ABOUT THIS DEMO:");
+        c.igBulletText("Sections below are demonstrating many aspects of the library.");
+        c.igBulletText("The \"Examples\" menu above leads to more demo contents.");
+        c.igBulletText("The \"Tools\" menu above gives access to: About Box, Style Editor,\n" ++
+            "and Metrics/Debugger (general purpose Dear ImGui debugging tool).");
+        c.igSeparator();
+
+        c.igText("PROGRAMMER GUIDE:");
+        c.igBulletText("See the ShowDemoWindow() code in imgui_demo.cpp. <- you are here!");
+        c.igBulletText("See comments in imgui.cpp.");
+        c.igBulletText("See example applications in the examples/ folder.");
+        c.igBulletText("Read the FAQ at http://www.dearimgui.org/faq/");
+        c.igBulletText("Set 'io.ConfigFlags |= NavEnableKeyboard' for keyboard controls.");
+        c.igBulletText("Set 'io.ConfigFlags |= NavEnableGamepad' for gamepad controls.");
+        c.igSeparator();
+
+        c.igText("USER GUIDE:");
+        c.igShowUserGuide();
+    }
+
+    _ = c.ImGuiTableColumnSettings_ImGuiTableColumnSettings();
+
+    c.igText("Number is %d", .{static.number});
+    static.number += 1;
+
+    c.igPopItemWidth();
+    c.igEnd();
 }
 
 fn draw(demo: *DemoState) void {
