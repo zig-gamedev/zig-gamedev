beda3c98ee84cd62c4fb8c339eb6c0a8ab0d3c41
Michal Ziulek <michalziulek@gmail.com>
2021-11-09 17:19:05 +0100

    Updated build scripts.

--- build_all_samples.bat
+++ build_all_samples.bat
@@ -6,7 +6,6 @@ for /D %%G in (*) do (
     echo --- %%G --- output directory is "samples\%%G\zig-out\bin"
     if exist content\shaders\*.cso del content\shaders\*.cso
     if exist zig-out rmdir /s /q zig-out
-    if exist zig-cache rmdir /s /q zig-cache
     zig build
     cd..
 )
