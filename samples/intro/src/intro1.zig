26f2385ea025709ec234b1d8bed51920e1227a42
Michal Ziulek <michalziulek@gmail.com>
2022-01-14 13:22:45 +0100

    intro: Added project that will include few designed as tutorials.

---
+++ samples/intro/src/intro1.zig
@@ -0,0 +1,187 @@
+const std = @import("std");
+const win32 = @import("win32");
+const w = win32.base;
+const d2d1 = win32.d2d1;
+const d3d12 = win32.d3d12;
+const dwrite = win32.dwrite;
+const common = @import("common");
+const gfx = common.graphics;
+const lib = common.library;
+const zm = common.zmath;
+const c = common.c;
+const math = std.math;
+const assert = std.debug.assert;
+const hrPanic = lib.hrPanic;
+const hrPanicOnFail = lib.hrPanicOnFail;
+const L = std.unicode.utf8ToUtf16LeStringLiteral;
+
+pub export var D3D12SDKVersion: u32 = 4;
+pub export var D3D12SDKPath: [*:0]const u8 = ".\\d3d12\\";
+
+const window_name = "zig-gamedev: intro 1";
+const window_width = 1920;
+const window_height = 1080;
+
+const DemoState = struct {
+    gctx: gfx.GraphicsContext,
+    guictx: gfx.GuiContext,
+    frame_stats: lib.FrameStats,
+
+    brush: *d2d1.ISolidColorBrush,
+    normal_tfmt: *dwrite.ITextFormat,
+};
+
+fn init(gpa_allocator: std.mem.Allocator) DemoState {
+    const window = lib.initWindow(gpa_allocator, window_name, window_width, window_height) catch unreachable;
+
+    var arena_allocator_state = std.heap.ArenaAllocator.init(gpa_allocator);
+    defer arena_allocator_state.deinit();
+    const arena_allocator = arena_allocator_state.allocator();
+
+    var gctx = gfx.GraphicsContext.init(window);
+    gctx.present_flags = 0;
+    gctx.present_interval = 1;
+
+    const brush = blk: {
+        var brush: *d2d1.ISolidColorBrush = undefined;
+        hrPanicOnFail(gctx.d2d.context.CreateSolidColorBrush(
+            &.{ .r = 1.0, .g = 0.0, .b = 0.0, .a = 0.5 },
+            null,
+            @ptrCast(*?*d2d1.ISolidColorBrush, &brush),
+        ));
+        break :blk brush;
+    };
+
+    const normal_tfmt = blk: {
+        var info_txtfmt: *dwrite.ITextFormat = undefined;
+        hrPanicOnFail(gctx.dwrite_factory.CreateTextFormat(
+            L("Verdana"),
+            null,
+            dwrite.FONT_WEIGHT.BOLD,
+            dwrite.FONT_STYLE.NORMAL,
+            dwrite.FONT_STRETCH.NORMAL,
+            32.0,
+            L("en-us"),
+            @ptrCast(*?*dwrite.ITextFormat, &info_txtfmt),
+        ));
+        break :blk info_txtfmt;
+    };
+    hrPanicOnFail(normal_tfmt.SetTextAlignment(.LEADING));
+    hrPanicOnFail(normal_tfmt.SetParagraphAlignment(.NEAR));
+
+    //
+    // Begin frame to init/upload resources to the GPU.
+    //
+    gctx.beginFrame();
+
+    var guictx = gfx.GuiContext.init(arena_allocator, &gctx, 1);
+
+    gctx.endFrame();
+    gctx.finishGpuCommands();
+
+    return .{
+        .gctx = gctx,
+        .guictx = guictx,
+        .frame_stats = lib.FrameStats.init(),
+        .brush = brush,
+        .normal_tfmt = normal_tfmt,
+    };
+}
+
+fn deinit(demo: *DemoState, gpa_allocator: std.mem.Allocator) void {
+    demo.gctx.finishGpuCommands();
+    _ = demo.brush.Release();
+    _ = demo.normal_tfmt.Release();
+    demo.guictx.deinit(&demo.gctx);
+    demo.gctx.deinit();
+    lib.deinitWindow(gpa_allocator);
+    demo.* = undefined;
+}
+
+fn update(demo: *DemoState) void {
+    demo.frame_stats.update();
+    const dt = demo.frame_stats.delta_time;
+    lib.newImGuiFrame(dt);
+}
+
+fn draw(demo: *DemoState) void {
+    var gctx = &demo.gctx;
+    gctx.beginFrame();
+
+    const back_buffer = gctx.getBackBuffer();
+    gctx.addTransitionBarrier(back_buffer.resource_handle, d3d12.RESOURCE_STATE_RENDER_TARGET);
+    gctx.flushResourceBarriers();
+
+    gctx.cmdlist.OMSetRenderTargets(
+        1,
+        &[_]d3d12.CPU_DESCRIPTOR_HANDLE{back_buffer.descriptor_handle},
+        w.TRUE,
+        null,
+    );
+    gctx.cmdlist.ClearRenderTargetView(
+        back_buffer.descriptor_handle,
+        &[4]f32{ 0.0, 0.0, 0.0, 1.0 },
+        0,
+        null,
+    );
+
+    demo.guictx.draw(gctx);
+
+    gctx.beginDraw2d();
+    {
+        const stats = &demo.frame_stats;
+        var buffer = [_]u8{0} ** 64;
+        const text = std.fmt.bufPrint(
+            buffer[0..],
+            "FPS: {d:.1}\nCPU time: {d:.3} ms",
+            .{ stats.fps, stats.average_cpu_time },
+        ) catch unreachable;
+
+        demo.brush.SetColor(&.{ .r = 1.0, .g = 1.0, .b = 1.0, .a = 1.0 });
+        lib.drawText(
+            gctx.d2d.context,
+            text,
+            demo.normal_tfmt,
+            &d2d1.RECT_F{
+                .left = 10.0,
+                .top = 10.0,
+                .right = @intToFloat(f32, gctx.viewport_width),
+                .bottom = @intToFloat(f32, gctx.viewport_height),
+            },
+            @ptrCast(*d2d1.IBrush, demo.brush),
+        );
+    }
+    gctx.endDraw2d();
+
+    gctx.endFrame();
+}
+
+pub fn main() !void {
+    lib.init();
+    defer lib.deinit();
+
+    var gpa_allocator_state = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const leaked = gpa_allocator_state.deinit();
+        std.debug.assert(leaked == false);
+    }
+    const gpa_allocator = gpa_allocator_state.allocator();
+
+    var demo = init(gpa_allocator);
+    defer deinit(&demo, gpa_allocator);
+
+    while (true) {
+        var message = std.mem.zeroes(w.user32.MSG);
+        const has_message = w.user32.peekMessageA(&message, null, 0, 0, w.user32.PM_REMOVE) catch false;
+        if (has_message) {
+            _ = w.user32.translateMessage(&message);
+            _ = w.user32.dispatchMessageA(&message);
+            if (message.message == w.user32.WM_QUIT) {
+                break;
+            }
+        } else {
+            update(&demo);
+            draw(&demo);
+        }
+    }
+}
