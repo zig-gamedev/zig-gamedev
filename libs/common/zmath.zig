be64488ce8422f1f5b958301e9f53e94665424a7
Michal Ziulek <michalziulek@gmail.com>
2022-02-06 00:52:19 +0100

    zmath: Added fft16() + test.

--- libs/common/zmath.zig
+++ libs/common/zmath.zig
@@ -3135,6 +3135,118 @@ test "zmath.fft8" {
     try expect(approxEqAbs(im[1], f32x4(0.0, -1.656854, -4.0, -9.656854), epsilon));
 }
 
+pub fn fft16(re: []F32x4, im: []F32x4, count: u32) void {
+    assert(std.math.isPowerOfTwo(count));
+    assert(re.len == 4 * count);
+    assert(im.len == 4 * count);
+
+    const static = struct {
+        const unity_table_re = [4]F32x4{
+            f32x4(1.0, 1.0, 1.0, 1.0),
+            f32x4(1.0, 0.92387950, 0.70710677, 0.38268343),
+            f32x4(1.0, 0.70710677, -4.3711388e-008, -0.70710677),
+            f32x4(1.0, 0.38268343, -0.70710677, -0.92387950),
+        };
+        const unity_table_im = [4]F32x4{
+            f32x4(-0.0, -0.0, -0.0, -0.0),
+            f32x4(-0.0, -0.38268343, -0.70710677, -0.92387950),
+            f32x4(-0.0, -0.70710677, -1.0, -0.70710677),
+            f32x4(-0.0, -0.92387950, -0.70710677, 0.38268343),
+        };
+    };
+
+    var index: u32 = 0;
+    while (index < count) : (index += 1) {
+        fftButterflyDit4_4(
+            &re[index * 4],
+            &re[index * 4 + 1],
+            &re[index * 4 + 2],
+            &re[index * 4 + 3],
+            &im[index * 4],
+            &im[index * 4 + 1],
+            &im[index * 4 + 2],
+            &im[index * 4 + 3],
+            static.unity_table_re[0..],
+            static.unity_table_im[0..],
+            1,
+            true,
+        );
+    }
+}
+test "zmath.fft16" {
+    const epsilon = 0.0001;
+    var re = [_]F32x4{
+        f32x4(1.0, 2.0, 3.0, 4.0),
+        f32x4(5.0, 6.0, 7.0, 8.0),
+        f32x4(9.0, 10.0, 11.0, 12.0),
+        f32x4(13.0, 14.0, 15.0, 16.0),
+    };
+    var im = [_]F32x4{ f32x4s(0.0), f32x4s(0.0), f32x4s(0.0), f32x4s(0.0) };
+    fft16(re[0..], im[0..], 1);
+
+    var re_uns: [4]F32x4 = undefined;
+    var im_uns: [4]F32x4 = undefined;
+    fftUnswizzle(re[0..], re_uns[0..]);
+    fftUnswizzle(im[0..], im_uns[0..]);
+
+    try expect(std.math.log2_int(u32, 8) == 3);
+
+    try expect(approxEqAbs(re_uns[0], f32x4(136.0, -8.0, -8.0, -8.0), epsilon));
+    try expect(approxEqAbs(re_uns[1], f32x4(-8.0, -8.0, -8.0, -8.0), epsilon));
+    try expect(approxEqAbs(re_uns[2], f32x4(-8.0, -8.0, -8.0, -8.0), epsilon));
+    try expect(approxEqAbs(re_uns[3], f32x4(-8.0, -8.0, -8.0, -8.0), epsilon));
+    try expect(approxEqAbs(im_uns[0], f32x4(0.0, 40.218716, 19.313708, 11.972846), epsilon));
+    try expect(approxEqAbs(im_uns[1], f32x4(8.0, 5.345429, 3.313708, 1.591299), epsilon));
+    try expect(approxEqAbs(im_uns[2], f32x4(0.0, -1.591299, -3.313708, -5.345429), epsilon));
+    try expect(approxEqAbs(im_uns[3], f32x4(-8.0, -11.972846, -19.313708, -40.218716), epsilon));
+}
+
+fn fftUnswizzle(input: []const F32x4, output: []F32x4) void {
+    assert(std.math.isPowerOfTwo(input.len));
+    assert(input.len == output.len);
+
+    const log2_length = std.math.log2_int(u32, @intCast(u32, input.len * 4));
+    const length = @intCast(u32, input.len);
+
+    const f32_output = @ptrCast([*]f32, output.ptr)[0 .. output.len * 4];
+
+    const static = struct {
+        const swizzle_table = [256]u8{
+            0x00, 0x40, 0x80, 0xC0, 0x10, 0x50, 0x90, 0xD0, 0x20, 0x60, 0xA0, 0xE0, 0x30, 0x70, 0xB0, 0xF0,
+            0x04, 0x44, 0x84, 0xC4, 0x14, 0x54, 0x94, 0xD4, 0x24, 0x64, 0xA4, 0xE4, 0x34, 0x74, 0xB4, 0xF4,
+            0x08, 0x48, 0x88, 0xC8, 0x18, 0x58, 0x98, 0xD8, 0x28, 0x68, 0xA8, 0xE8, 0x38, 0x78, 0xB8, 0xF8,
+            0x0C, 0x4C, 0x8C, 0xCC, 0x1C, 0x5C, 0x9C, 0xDC, 0x2C, 0x6C, 0xAC, 0xEC, 0x3C, 0x7C, 0xBC, 0xFC,
+            0x01, 0x41, 0x81, 0xC1, 0x11, 0x51, 0x91, 0xD1, 0x21, 0x61, 0xA1, 0xE1, 0x31, 0x71, 0xB1, 0xF1,
+            0x05, 0x45, 0x85, 0xC5, 0x15, 0x55, 0x95, 0xD5, 0x25, 0x65, 0xA5, 0xE5, 0x35, 0x75, 0xB5, 0xF5,
+            0x09, 0x49, 0x89, 0xC9, 0x19, 0x59, 0x99, 0xD9, 0x29, 0x69, 0xA9, 0xE9, 0x39, 0x79, 0xB9, 0xF9,
+            0x0D, 0x4D, 0x8D, 0xCD, 0x1D, 0x5D, 0x9D, 0xDD, 0x2D, 0x6D, 0xAD, 0xED, 0x3D, 0x7D, 0xBD, 0xFD,
+            0x02, 0x42, 0x82, 0xC2, 0x12, 0x52, 0x92, 0xD2, 0x22, 0x62, 0xA2, 0xE2, 0x32, 0x72, 0xB2, 0xF2,
+            0x06, 0x46, 0x86, 0xC6, 0x16, 0x56, 0x96, 0xD6, 0x26, 0x66, 0xA6, 0xE6, 0x36, 0x76, 0xB6, 0xF6,
+            0x0A, 0x4A, 0x8A, 0xCA, 0x1A, 0x5A, 0x9A, 0xDA, 0x2A, 0x6A, 0xAA, 0xEA, 0x3A, 0x7A, 0xBA, 0xFA,
+            0x0E, 0x4E, 0x8E, 0xCE, 0x1E, 0x5E, 0x9E, 0xDE, 0x2E, 0x6E, 0xAE, 0xEE, 0x3E, 0x7E, 0xBE, 0xFE,
+            0x03, 0x43, 0x83, 0xC3, 0x13, 0x53, 0x93, 0xD3, 0x23, 0x63, 0xA3, 0xE3, 0x33, 0x73, 0xB3, 0xF3,
+            0x07, 0x47, 0x87, 0xC7, 0x17, 0x57, 0x97, 0xD7, 0x27, 0x67, 0xA7, 0xE7, 0x37, 0x77, 0xB7, 0xF7,
+            0x0B, 0x4B, 0x8B, 0xCB, 0x1B, 0x5B, 0x9B, 0xDB, 0x2B, 0x6B, 0xAB, 0xEB, 0x3B, 0x7B, 0xBB, 0xFB,
+            0x0F, 0x4F, 0x8F, 0xCF, 0x1F, 0x5F, 0x9F, 0xDF, 0x2F, 0x6F, 0xAF, 0xEF, 0x3F, 0x7F, 0xBF, 0xFF,
+        };
+    };
+
+    const rev32 = @intCast(u5, @as(u32, 32) - log2_length);
+    var index: u32 = 0;
+    while (index < length) : (index += 1) {
+        const n = index * 4;
+        const addr =
+            (@intCast(u32, static.swizzle_table[n & 0xff]) << 24) |
+            (@intCast(u32, static.swizzle_table[(n >> 8) & 0xff]) << 16) |
+            (@intCast(u32, static.swizzle_table[(n >> 16) & 0xff]) << 8) |
+            @intCast(u32, static.swizzle_table[(n >> 24) & 0xff]);
+        f32_output[addr >> rev32] = input[index][0];
+        f32_output[(0x40000000 | addr) >> rev32] = input[index][1];
+        f32_output[(0x80000000 | addr) >> rev32] = input[index][2];
+        f32_output[(0xC0000000 | addr) >> rev32] = input[index][3];
+    }
+}
+
 fn fftInitUnityTable(out_unity_table: []F32x4) void {
     assert(std.math.isPowerOfTwo(out_unity_table.len));
     assert(out_unity_table.len >= 32 and out_unity_table.len <= 512);
