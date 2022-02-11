66b0f21d6b90dfcda517d6ba334a17344370efed
Michal Ziulek <michalziulek@gmail.com>
2022-02-11 15:22:51 +0100

    zbullet: Experimenting with the API.

--- libs/zbullet/src/zbullet.zig
+++ libs/zbullet/src/zbullet.zig
@@ -1,11 +1,38 @@
 const std = @import("std");
+const expect = std.testing.expect;
 const c = @cImport({
     @cInclude("cbullet.h");
 });
 
-test "bt" {
-    const w = c.cbtWorldCreate();
-    c.cbtWorldDestroy(w);
+pub const Vector3 = [3]f32;
 
-    std.debug.print("zbullet\n", .{});
+pub const World = struct {
+    handle: c.CbtWorldHandle,
+
+    pub fn create() World {
+        return .{ .handle = c.cbtWorldCreate() };
+    }
+
+    pub fn destroy(world: World) void {
+        c.cbtWorldDestroy(world.handle);
+    }
+
+    pub fn setGravity(world: World, gravity: Vector3) void {
+        c.cbtWorldSetGravity(world.handle, &gravity);
+    }
+
+    pub fn getGravity(world: World) Vector3 {
+        var gravity: Vector3 = undefined;
+        c.cbtWorldGetGravity(world.handle, &gravity);
+        return gravity;
+    }
+};
+
+test "zbullet.world.gravity" {
+    const world = World.create();
+    defer world.destroy();
+
+    world.setGravity(.{ 0.0, -10.0, 0.0 });
+    const gravity = world.getGravity();
+    try expect(gravity[0] == 0.0 and gravity[1] == -10.0 and gravity[2] == 0.0);
 }
