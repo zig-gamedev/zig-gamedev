7776d8cb9fc699fe367d360980d8ffeb7c0a9c39
Michal Ziulek <michalziulek@gmail.com>
2022-03-20 17:57:01 +0100

    Added 'procedural mesh' demo (wip).

---
+++ samples/procedural_mesh/build.zig
@@ -0,0 +1,147 @@
+const std = @import("std");
+
+const Options = @import("../../build.zig").Options;
+const content_dir = "procedural_mesh_content/";
+
+pub fn build(b: *std.build.Builder, options: Options) *std.build.LibExeObjStep {
+    const exe_options = b.addOptions();
+    exe_options.addOption(bool, "enable_pix", options.enable_pix);
+    exe_options.addOption(bool, "enable_dx_debug", options.enable_dx_debug);
+    exe_options.addOption(bool, "enable_dx_gpu_debug", options.enable_dx_gpu_debug);
+    exe_options.addOption(bool, "enable_tracy", options.tracy != null);
+    exe_options.addOption(bool, "enable_d2d", false);
+    exe_options.addOption([]const u8, "content_dir", content_dir);
+
+    const exe = b.addExecutable("procedural_mesh", thisDir() ++ "/src/procedural_mesh.zig");
+    exe.setBuildMode(options.build_mode);
+    exe.setTarget(options.target);
+    exe.addOptions("build_options", exe_options);
+
+    const dxc_step = buildShaders(b);
+    const install_content_step = b.addInstallDirectory(.{
+        .source_dir = thisDir() ++ "/" ++ content_dir,
+        .install_dir = .{ .custom = "" },
+        .install_subdir = "bin/" ++ content_dir,
+    });
+    install_content_step.step.dependOn(dxc_step);
+    exe.step.dependOn(&install_content_step.step);
+
+    // This is needed to export symbols from an .exe file.
+    // We export D3D12SDKVersion and D3D12SDKPath symbols which
+    // is required by DirectX 12 Agility SDK.
+    exe.rdynamic = true;
+    exe.want_lto = false;
+
+    const options_pkg = std.build.Pkg{
+        .name = "build_options",
+        .path = exe_options.getSource(),
+    };
+
+    const zwin32_pkg = std.build.Pkg{
+        .name = "zwin32",
+        .path = .{ .path = thisDir() ++ "/../../libs/zwin32/zwin32.zig" },
+    };
+    exe.addPackage(zwin32_pkg);
+
+    const ztracy_pkg = std.build.Pkg{
+        .name = "ztracy",
+        .path = .{ .path = thisDir() ++ "/../../libs/ztracy/src/ztracy.zig" },
+        .dependencies = &[_]std.build.Pkg{options_pkg},
+    };
+    exe.addPackage(ztracy_pkg);
+    @import("../../libs/ztracy/build.zig").link(b, exe, .{ .tracy_path = options.tracy });
+
+    const zd3d12_pkg = std.build.Pkg{
+        .name = "zd3d12",
+        .path = .{ .path = thisDir() ++ "/../../libs/zd3d12/src/zd3d12.zig" },
+        .dependencies = &[_]std.build.Pkg{
+            zwin32_pkg,
+            ztracy_pkg,
+            options_pkg,
+        },
+    };
+    exe.addPackage(zd3d12_pkg);
+    @import("../../libs/zd3d12/build.zig").link(b, exe);
+
+    const zmath_pkg = std.build.Pkg{
+        .name = "zmath",
+        .path = .{ .path = thisDir() ++ "/../../libs/zmath/zmath.zig" },
+    };
+    exe.addPackage(zmath_pkg);
+
+    const zmesh_pkg = std.build.Pkg{
+        .name = "zmesh",
+        .path = .{ .path = thisDir() ++ "/../../libs/zmesh/src/zmesh.zig" },
+    };
+    exe.addPackage(zmesh_pkg);
+    @import("../../libs/zmesh/build.zig").link(b, exe);
+
+    const common_pkg = std.build.Pkg{
+        .name = "common",
+        .path = .{ .path = thisDir() ++ "/../../libs/common/src/common.zig" },
+        .dependencies = &[_]std.build.Pkg{
+            zwin32_pkg,
+            zd3d12_pkg,
+            ztracy_pkg,
+            options_pkg,
+        },
+    };
+    exe.addPackage(common_pkg);
+    @import("../../libs/common/build.zig").link(b, exe);
+
+    return exe;
+}
+
+fn buildShaders(b: *std.build.Builder) *std.build.Step {
+    const dxc_step = b.step("procedural_mesh-dxc", "Build shaders for 'procedural mesh' demo");
+
+    var dxc_command = makeDxcCmd(
+        "../../libs/common/src/hlsl/common.hlsl",
+        "vsImGui",
+        "imgui.vs.cso",
+        "vs",
+        "PSO__IMGUI",
+    );
+    dxc_step.dependOn(&b.addSystemCommand(&dxc_command).step);
+    dxc_command = makeDxcCmd(
+        "../../libs/common/src/hlsl/common.hlsl",
+        "psImGui",
+        "imgui.ps.cso",
+        "ps",
+        "PSO__IMGUI",
+    );
+    dxc_step.dependOn(&b.addSystemCommand(&dxc_command).step);
+
+    //dxc_command = makeDxcCmd("src/procedural_mesh.hlsl", "vsMain", "lines.vs.cso", "vs", "");
+    //dxc_step.dependOn(&b.addSystemCommand(&dxc_command).step);
+    //dxc_command = makeDxcCmd("src/procedural_mesh.hlsl", "psMain", "lines.ps.cso", "ps", "");
+    //dxc_step.dependOn(&b.addSystemCommand(&dxc_command).step);
+
+    return dxc_step;
+}
+
+fn makeDxcCmd(
+    comptime input_path: []const u8,
+    comptime entry_point: []const u8,
+    comptime output_filename: []const u8,
+    comptime profile: []const u8,
+    comptime define: []const u8,
+) [9][]const u8 {
+    const shader_ver = "6_6";
+    const shader_dir = thisDir() ++ "/" ++ content_dir ++ "shaders/";
+    return [9][]const u8{
+        thisDir() ++ "/../../libs/zwin32/bin/x64/dxc.exe",
+        thisDir() ++ "/" ++ input_path,
+        "/E " ++ entry_point,
+        "/Fo " ++ shader_dir ++ output_filename,
+        "/T " ++ profile ++ "_" ++ shader_ver,
+        if (define.len == 0) "" else "/D " ++ define,
+        "/WX",
+        "/Ges",
+        "/O3",
+    };
+}
+
+fn thisDir() []const u8 {
+    return std.fs.path.dirname(@src().file) orelse ".";
+}
---
+++ samples/procedural_mesh/src/procedural_mesh.zig
@@ -0,0 +1,298 @@
+const std = @import("std");
+const math = std.math;
+const assert = std.debug.assert;
+const L = std.unicode.utf8ToUtf16LeStringLiteral;
+const zwin32 = @import("zwin32");
+const w32 = zwin32.base;
+const d3d12 = zwin32.d3d12;
+const hrPanic = zwin32.hrPanic;
+const hrPanicOnFail = zwin32.hrPanicOnFail;
+const zd3d12 = @import("zd3d12");
+const common = @import("common");
+const GuiRenderer = common.GuiRenderer;
+const c = common.c;
+const zm = @import("zmath");
+const zmesh = @import("zmesh");
+
+pub export const D3D12SDKVersion: u32 = 4;
+pub export const D3D12SDKPath: [*:0]const u8 = ".\\d3d12\\";
+
+const content_dir = @import("build_options").content_dir;
+
+const window_name = "zig-gamedev: procedural mesh";
+const window_width = 1920;
+const window_height = 1080;
+
+const Pso_DrawConst = struct {
+    object_to_world: [16]f32,
+};
+
+const Pso_FrameConst = struct {
+    world_to_clip: [16]f32,
+};
+
+const Pso_Vertex = struct {
+    position: [3]f32,
+    color: [3]f32,
+};
+
+const DemoState = struct {
+    gctx: zd3d12.GraphicsContext,
+    guir: GuiRenderer,
+    frame_stats: common.FrameStats,
+
+    vertex_buffer: zd3d12.ResourceHandle,
+    index_buffer: zd3d12.ResourceHandle,
+
+    depth_texture: zd3d12.ResourceHandle,
+    depth_texture_dsv: d3d12.CPU_DESCRIPTOR_HANDLE,
+
+    camera: struct {
+        position: [3]f32 = .{ -10.0, 15.0, -10.0 },
+        forward: [3]f32 = .{ 0.0, 0.0, 1.0 },
+        pitch: f32 = 0.15 * math.pi,
+        yaw: f32 = 0.25 * math.pi,
+    } = .{},
+    mouse: struct {
+        cursor_prev_x: i32 = 0,
+        cursor_prev_y: i32 = 0,
+    } = .{},
+};
+
+fn init(allocator: std.mem.Allocator) !DemoState {
+    const window = try common.initWindow(allocator, window_name, window_width, window_height);
+
+    var arena_allocator_state = std.heap.ArenaAllocator.init(allocator);
+    defer arena_allocator_state.deinit();
+    const arena_allocator = arena_allocator_state.allocator();
+
+    var gctx = zd3d12.GraphicsContext.init(allocator, window);
+    gctx.present_flags = 0;
+    gctx.present_interval = 1;
+
+    var mesh_indices = std.ArrayList(u16).init(arena_allocator);
+    var mesh_positions = std.ArrayList([3]f32).init(arena_allocator);
+    var mesh_normals = std.ArrayList([3]f32).init(arena_allocator);
+
+    {
+        var cube = zmesh.initCube();
+        defer cube.deinit();
+        cube.computeNormals();
+
+        mesh_indices.appendSlice(cube.indices) catch unreachable;
+        mesh_positions.appendSlice(cube.positions) catch unreachable;
+        mesh_normals.appendSlice(cube.normals.?) catch unreachable;
+    }
+
+    const mesh_num_vertices = @intCast(u32, mesh_positions.items.len);
+    const mesh_num_indices = @intCast(u32, mesh_indices.items.len);
+
+    const vertex_buffer = gctx.createCommittedResource(
+        .DEFAULT,
+        d3d12.HEAP_FLAG_NONE,
+        &d3d12.RESOURCE_DESC.initBuffer(mesh_num_vertices * @sizeOf(Pso_Vertex)),
+        d3d12.RESOURCE_STATE_COPY_DEST,
+        null,
+    ) catch |err| hrPanic(err);
+
+    const index_buffer = gctx.createCommittedResource(
+        .DEFAULT,
+        d3d12.HEAP_FLAG_NONE,
+        &d3d12.RESOURCE_DESC.initBuffer(mesh_num_indices * @sizeOf(u32)),
+        d3d12.RESOURCE_STATE_COPY_DEST,
+        null,
+    ) catch |err| hrPanic(err);
+
+    // Create depth texture resource.
+    const depth_texture = gctx.createCommittedResource(
+        .DEFAULT,
+        d3d12.HEAP_FLAG_NONE,
+        &blk: {
+            var desc = d3d12.RESOURCE_DESC.initTex2d(.D32_FLOAT, gctx.viewport_width, gctx.viewport_height, 1);
+            desc.Flags = d3d12.RESOURCE_FLAG_ALLOW_DEPTH_STENCIL | d3d12.RESOURCE_FLAG_DENY_SHADER_RESOURCE;
+            break :blk desc;
+        },
+        d3d12.RESOURCE_STATE_DEPTH_WRITE,
+        &d3d12.CLEAR_VALUE.initDepthStencil(.D32_FLOAT, 1.0, 0),
+    ) catch |err| hrPanic(err);
+
+    // Create depth texture view.
+    const depth_texture_dsv = gctx.allocateCpuDescriptors(.DSV, 1);
+    gctx.device.CreateDepthStencilView(
+        gctx.lookupResource(depth_texture).?,
+        null,
+        depth_texture_dsv,
+    );
+
+    //
+    // Begin frame to init/upload resources to the GPU.
+    //
+    gctx.beginFrame();
+
+    var guir = GuiRenderer.init(arena_allocator, &gctx, 1, content_dir);
+
+    gctx.endFrame();
+    gctx.finishGpuCommands();
+
+    return DemoState{
+        .gctx = gctx,
+        .guir = guir,
+        .frame_stats = common.FrameStats.init(),
+        .vertex_buffer = vertex_buffer,
+        .index_buffer = index_buffer,
+        .depth_texture = depth_texture,
+        .depth_texture_dsv = depth_texture_dsv,
+    };
+}
+
+fn deinit(demo: *DemoState, allocator: std.mem.Allocator) void {
+    demo.gctx.finishGpuCommands();
+    demo.guir.deinit(&demo.gctx);
+    demo.gctx.deinit(allocator);
+    common.deinitWindow(allocator);
+    demo.* = undefined;
+}
+
+fn update(demo: *DemoState) void {
+    demo.frame_stats.update(demo.gctx.window, window_name);
+    const dt = demo.frame_stats.delta_time;
+    common.newImGuiFrame(dt);
+
+    c.igSetNextWindowPos(
+        c.ImVec2{ .x = @intToFloat(f32, demo.gctx.viewport_width) - 600.0 - 20, .y = 20.0 },
+        c.ImGuiCond_FirstUseEver,
+        c.ImVec2{ .x = 0.0, .y = 0.0 },
+    );
+    c.igSetNextWindowSize(.{ .x = 600.0, .y = -1 }, c.ImGuiCond_Always);
+
+    _ = c.igBegin(
+        "Demo Settings",
+        null,
+        c.ImGuiWindowFlags_NoMove | c.ImGuiWindowFlags_NoResize | c.ImGuiWindowFlags_NoSavedSettings,
+    );
+    c.igBulletText("", "");
+    c.igSameLine(0, -1);
+    c.igTextColored(.{ .x = 0, .y = 0.8, .z = 0, .w = 1 }, "Right Mouse Button + drag", "");
+    c.igSameLine(0, -1);
+    c.igText(" :  rotate camera", "");
+
+    c.igBulletText("", "");
+    c.igSameLine(0, -1);
+    c.igTextColored(.{ .x = 0, .y = 0.8, .z = 0, .w = 1 }, "W, A, S, D", "");
+    c.igSameLine(0, -1);
+    c.igText(" :  move camera", "");
+
+    c.igEnd();
+
+    // Handle camera rotation with mouse.
+    {
+        var pos: w32.POINT = undefined;
+        _ = w32.GetCursorPos(&pos);
+        const delta_x = @intToFloat(f32, pos.x) - @intToFloat(f32, demo.mouse.cursor_prev_x);
+        const delta_y = @intToFloat(f32, pos.y) - @intToFloat(f32, demo.mouse.cursor_prev_y);
+        demo.mouse.cursor_prev_x = pos.x;
+        demo.mouse.cursor_prev_y = pos.y;
+
+        if (w32.GetAsyncKeyState(w32.VK_RBUTTON) < 0) {
+            demo.camera.pitch += 0.0025 * delta_y;
+            demo.camera.yaw += 0.0025 * delta_x;
+            demo.camera.pitch = math.min(demo.camera.pitch, 0.48 * math.pi);
+            demo.camera.pitch = math.max(demo.camera.pitch, -0.48 * math.pi);
+            demo.camera.yaw = zm.modAngle(demo.camera.yaw);
+        }
+    }
+
+    // Handle camera movement with 'WASD' keys.
+    {
+        const speed = zm.f32x4s(10.0);
+        const delta_time = zm.f32x4s(demo.frame_stats.delta_time);
+        const transform = zm.mul(zm.rotationX(demo.camera.pitch), zm.rotationY(demo.camera.yaw));
+        var forward = zm.normalize3(zm.mul(zm.f32x4(0.0, 0.0, 1.0, 0.0), transform));
+
+        zm.store(demo.camera.forward[0..], forward, 3);
+
+        const right = speed * delta_time * zm.normalize3(zm.cross3(zm.f32x4(0.0, 1.0, 0.0, 0.0), forward));
+        forward = speed * delta_time * forward;
+
+        var cpos = zm.load(demo.camera.position[0..], zm.Vec, 3);
+
+        if (w32.GetAsyncKeyState('W') < 0) {
+            cpos += forward;
+        } else if (w32.GetAsyncKeyState('S') < 0) {
+            cpos -= forward;
+        }
+        if (w32.GetAsyncKeyState('D') < 0) {
+            cpos += right;
+        } else if (w32.GetAsyncKeyState('A') < 0) {
+            cpos -= right;
+        }
+
+        zm.store(demo.camera.position[0..], cpos, 3);
+    }
+}
+
+fn draw(demo: *DemoState) void {
+    var gctx = &demo.gctx;
+
+    const cam_world_to_view = zm.lookToLh(
+        zm.load(demo.camera.position[0..], zm.Vec, 3),
+        zm.load(demo.camera.forward[0..], zm.Vec, 3),
+        zm.f32x4(0.0, 1.0, 0.0, 0.0),
+    );
+    const cam_view_to_clip = zm.perspectiveFovLh(
+        0.25 * math.pi,
+        @intToFloat(f32, gctx.viewport_width) / @intToFloat(f32, gctx.viewport_height),
+        0.01,
+        200.0,
+    );
+    const cam_world_to_clip = zm.mul(cam_world_to_view, cam_view_to_clip);
+    _ = cam_world_to_clip;
+
+    gctx.beginFrame();
+
+    const back_buffer = gctx.getBackBuffer();
+    gctx.addTransitionBarrier(back_buffer.resource_handle, d3d12.RESOURCE_STATE_RENDER_TARGET);
+    gctx.flushResourceBarriers();
+
+    gctx.cmdlist.OMSetRenderTargets(
+        1,
+        &[_]d3d12.CPU_DESCRIPTOR_HANDLE{back_buffer.descriptor_handle},
+        w32.TRUE,
+        &demo.depth_texture_dsv,
+    );
+    gctx.cmdlist.ClearRenderTargetView(
+        back_buffer.descriptor_handle,
+        &[4]f32{ 0.0, 0.0, 0.0, 1.0 },
+        0,
+        null,
+    );
+    gctx.cmdlist.ClearDepthStencilView(demo.depth_texture_dsv, d3d12.CLEAR_FLAG_DEPTH, 1.0, 0, 0, null);
+
+    demo.guir.draw(gctx);
+
+    gctx.addTransitionBarrier(back_buffer.resource_handle, d3d12.RESOURCE_STATE_PRESENT);
+    gctx.flushResourceBarriers();
+
+    gctx.endFrame();
+}
+
+pub fn main() !void {
+    common.init();
+    defer common.deinit();
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    zmesh.init(allocator);
+    defer zmesh.deinit();
+
+    var demo = try init(allocator);
+    defer deinit(&demo, allocator);
+
+    while (common.handleWindowEvents()) {
+        update(&demo);
+        draw(&demo);
+    }
+}
