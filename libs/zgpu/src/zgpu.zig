8341dad26d3cb5a15aa9417f75841174aaaf87bd
Michal Ziulek <michalziulek@gmail.com>
2022-04-05 14:38:25 +0200

    Moved common code to 'zgpu' lib.

--- libs/zgpu/src/zgpu.zig
+++ libs/zgpu/src/zgpu.zig
@@ -2,13 +2,21 @@ const std = @import("std");
 const glfw = @import("glfw");
 const gpu = @import("gpu");
 const c = @import("c.zig").c;
+const objc = @cImport({
+    @cInclude("objc/message.h");
+});
 
 pub const GraphicsContext = struct {
     native_instance: gpu.NativeInstance,
     adapter_type: gpu.Adapter.Type,
     backend_type: gpu.Adapter.BackendType,
-    //device: gpu.Device,
+    device: gpu.Device,
     window: glfw.Window,
+    window_surface: gpu.Surface,
+    swap_chain: ?gpu.SwapChain,
+    swap_chain_format: gpu.Texture.Format,
+    swap_chain_descriptor_current: gpu.SwapChain.Descriptor,
+    swap_chain_descriptor_target: gpu.SwapChain.Descriptor,
 
     pub fn init(window: glfw.Window) GraphicsContext {
         c.dawnProcSetProcs(c.machDawnNativeGetProcs());
@@ -28,20 +36,137 @@ pub const GraphicsContext = struct {
             },
         };
 
-        // Print which adapter we are going to use.
         const props = backend_adapter.properties;
-        std.debug.print("zgpu: found {s} backend on {s} adapter: {s}, {s}\n", .{
+        std.debug.print("zgpu: Backend: '{s} ({s})' on adapter: '{s} ({s})' selected.\n", .{
             gpu.Adapter.backendTypeName(props.backend_type),
+            props.driver_description,
             gpu.Adapter.typeName(props.adapter_type),
             props.name,
-            props.driver_description,
         });
+
+        const device = switch (backend_adapter.waitForDevice(&.{})) {
+            .device => |v| v,
+            .err => |err| {
+                std.debug.print("failed to get device: error={} {s}\n", .{ err.code, err.message });
+                std.process.exit(1);
+            },
+        };
+
+        const window_surface = createSurfaceForWindow(
+            &native_instance,
+            window,
+            comptime detectGLFWOptions(),
+        );
+
+        const framebuffer_size = window.getFramebufferSize() catch unreachable;
+        const swap_chain_format = .bgra8_unorm;
+        const swap_chain_descriptor = gpu.SwapChain.Descriptor{
+            .label = "main window swap chain",
+            .usage = .render_attachment,
+            .format = swap_chain_format,
+            .width = framebuffer_size.width,
+            .height = framebuffer_size.height,
+            .present_mode = .fifo,
+            .implementation = 0,
+        };
 
         return GraphicsContext{
             .native_instance = native_instance,
             .adapter_type = props.adapter_type,
             .backend_type = props.backend_type,
+            .device = device,
             .window = window,
+            .window_surface = window_surface,
+            .swap_chain = null,
+            .swap_chain_format = swap_chain_format,
+            .swap_chain_descriptor_current = swap_chain_descriptor,
+            .swap_chain_descriptor_target = swap_chain_descriptor,
         };
     }
+
+    pub fn update(gctx: *GraphicsContext) void {
+        if (gctx.swap_chain == null or
+            !gctx.swap_chain_descriptor_current.equal(&gctx.swap_chain_descriptor_target))
+        {
+            gctx.swap_chain = gctx.device.nativeCreateSwapChain(
+                gctx.window_surface,
+                &gctx.swap_chain_descriptor_target,
+            );
+        }
+    }
 };
+
+fn detectGLFWOptions() glfw.BackendOptions {
+    const target = @import("builtin").target;
+    if (target.isDarwin()) return .{ .cocoa = true };
+    return switch (target.os.tag) {
+        .windows => .{ .win32 = true },
+        .linux => .{ .x11 = true },
+        else => .{},
+    };
+}
+
+fn createSurfaceForWindow(
+    native_instance: *const gpu.NativeInstance,
+    window: glfw.Window,
+    comptime glfw_options: glfw.BackendOptions,
+) gpu.Surface {
+    const glfw_native = glfw.Native(glfw_options);
+    const descriptor = if (glfw_options.win32) gpu.Surface.Descriptor{
+        .windows_hwnd = .{
+            .label = "basic surface",
+            .hinstance = std.os.windows.kernel32.GetModuleHandleW(null).?,
+            .hwnd = glfw_native.getWin32Window(window),
+        },
+    } else if (glfw_options.x11) gpu.Surface.Descriptor{
+        .xlib = .{
+            .label = "basic surface",
+            .display = glfw_native.getX11Display(),
+            .window = glfw_native.getX11Window(window),
+        },
+    } else if (glfw_options.cocoa) blk: {
+        const ns_window = glfw_native.getCocoaWindow(window);
+        const ns_view = msgSend(ns_window, "contentView", .{}, *anyopaque); // [nsWindow contentView]
+
+        // Create a CAMetalLayer that covers the whole window that will be passed to CreateSurface.
+        msgSend(ns_view, "setWantsLayer:", .{true}, void); // [view setWantsLayer:YES]
+        const layer = msgSend(objc.objc_getClass("CAMetalLayer"), "layer", .{}, ?*anyopaque); // [CAMetalLayer layer]
+        if (layer == null) @panic("failed to create Metal layer");
+        msgSend(ns_view, "setLayer:", .{layer.?}, void); // [view setLayer:layer]
+
+        // Use retina if the window was created with retina support.
+        const scale_factor = msgSend(ns_window, "backingScaleFactor", .{}, f64); // [ns_window backingScaleFactor]
+        msgSend(layer.?, "setContentsScale:", .{scale_factor}, void); // [layer setContentsScale:scale_factor]
+
+        break :blk gpu.Surface.Descriptor{
+            .metal_layer = .{
+                .label = "basic surface",
+                .layer = layer.?,
+            },
+        };
+    } else if (glfw_options.wayland) {
+        @panic("Dawn does not yet have Wayland support, see https://bugs.chromium.org/p/dawn/issues/detail?id=1246&q=surface&can=2");
+    } else unreachable;
+
+    return native_instance.createSurface(&descriptor);
+}
+
+// Borrowed from https://github.com/hazeycode/zig-objcrt
+fn msgSend(obj: anytype, sel_name: [:0]const u8, args: anytype, comptime ReturnType: type) ReturnType {
+    const args_meta = @typeInfo(@TypeOf(args)).Struct.fields;
+
+    const FnType = switch (args_meta.len) {
+        0 => fn (@TypeOf(obj), objc.SEL) callconv(.C) ReturnType,
+        1 => fn (@TypeOf(obj), objc.SEL, args_meta[0].field_type) callconv(.C) ReturnType,
+        2 => fn (@TypeOf(obj), objc.SEL, args_meta[0].field_type, args_meta[1].field_type) callconv(.C) ReturnType,
+        3 => fn (@TypeOf(obj), objc.SEL, args_meta[0].field_type, args_meta[1].field_type, args_meta[2].field_type) callconv(.C) ReturnType,
+        4 => fn (@TypeOf(obj), objc.SEL, args_meta[0].field_type, args_meta[1].field_type, args_meta[2].field_type, args_meta[3].field_type) callconv(.C) ReturnType,
+        else => @compileError("Unsupported number of args"),
+    };
+
+    // NOTE: func is a var because making it const causes a compile error which I believe is a compiler bug
+    var func = @ptrCast(FnType, objc.objc_msgSend);
+    const sel = objc.sel_getUid(sel_name);
+
+    return @call(.{}, func, .{ obj, sel } ++ args);
+}
--- samples/triangle_wgpu/src/triangle_wgpu.zig
+++ samples/triangle_wgpu/src/triangle_wgpu.zig
@@ -1,69 +1,24 @@
 const std = @import("std");
-const sample_utils = @import("sample_utils.zig");
-const c = @import("c.zig").c;
 const glfw = @import("glfw");
 const gpu = @import("gpu");
 const zgpu = @import("zgpu");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    var allocator = gpa.allocator();
+    //var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    //var allocator = gpa.allocator();
 
-    const setup = try sample_utils.setup(allocator);
-    const framebuffer_size = try setup.window.getFramebufferSize();
+    try glfw.init(.{});
 
-    const window_data = try allocator.create(WindowData);
-    window_data.* = .{
-        .surface = null,
-        .swap_chain = null,
-        .swap_chain_format = undefined,
-        .current_desc = undefined,
-        .target_desc = undefined,
+    // Create the test window and discover adapters using it (esp. for OpenGL)
+    const hints = glfw.Window.Hints{
+        .client_api = .no_api,
+        .cocoa_retina_framebuffer = true,
     };
-    setup.window.setUserPointer(window_data);
+    const window = try glfw.Window.create(1280, 960, "zig-gamedev: triangle (WebGPU)", null, null, hints);
 
-    var gctx = zgpu.GraphicsContext.init(setup.window);
-    _ = gctx;
+    var gctx = zgpu.GraphicsContext.init(window);
 
-    // If targeting OpenGL, we can't use the newer WGPUSurface API. Instead, we need to use the
-    // older Dawn-specific API. https://bugs.chromium.org/p/dawn/issues/detail?id=269&q=surface&can=2
-    const use_legacy_api = setup.backend_type == .opengl or setup.backend_type == .opengles;
-    var descriptor: gpu.SwapChain.Descriptor = undefined;
-    if (!use_legacy_api) {
-        window_data.swap_chain_format = .bgra8_unorm;
-        descriptor = .{
-            .label = "basic swap chain",
-            .usage = .render_attachment,
-            .format = window_data.swap_chain_format,
-            .width = framebuffer_size.width,
-            .height = framebuffer_size.height,
-            .present_mode = .fifo,
-            .implementation = 0,
-        };
-        window_data.surface = sample_utils.createSurfaceForWindow(
-            &setup.native_instance,
-            setup.window,
-            comptime sample_utils.detectGLFWOptions(),
-        );
-    } else {
-        const binding = c.machUtilsCreateBinding(@enumToInt(setup.backend_type), @ptrCast(*c.GLFWwindow, setup.window.handle), @ptrCast(c.WGPUDevice, setup.device.ptr));
-        if (binding == null) {
-            @panic("failed to create Dawn backend binding");
-        }
-        descriptor = std.mem.zeroes(gpu.SwapChain.Descriptor);
-        descriptor.implementation = c.machUtilsBackendBinding_getSwapChainImplementation(binding);
-        window_data.swap_chain = setup.device.nativeCreateSwapChain(null, &descriptor);
-
-        window_data.swap_chain_format = @intToEnum(gpu.Texture.Format, @intCast(u32, c.machUtilsBackendBinding_getPreferredSwapChainTextureFormat(binding)));
-        window_data.swap_chain.?.configure(
-            window_data.swap_chain_format,
-            .render_attachment,
-            framebuffer_size.width,
-            framebuffer_size.height,
-        );
-    }
-    window_data.current_desc = descriptor;
-    window_data.target_desc = descriptor;
+    //window.setUserPointer(&gctx);
 
     const vs =
         \\ @stage(vertex) fn main(
@@ -77,7 +32,7 @@ pub fn main() !void {
         \\     return vec4<f32>(pos[VertexIndex], 0.0, 1.0);
         \\ }
     ;
-    const vs_module = setup.device.createShaderModule(&.{
+    const vs_module = gctx.device.createShaderModule(&.{
         .label = "my vertex shader",
         .code = .{ .wgsl = vs },
     });
@@ -87,7 +42,7 @@ pub fn main() !void {
         \\     return vec4<f32>(0.0, 0.8, 0.0, 1.0);
         \\ }
     ;
-    const fs_module = setup.device.createShaderModule(&.{
+    const fs_module = gctx.device.createShaderModule(&.{
         .label = "my fragment shader",
         .code = .{ .wgsl = fs },
     });
@@ -106,7 +61,7 @@ pub fn main() !void {
         },
     };
     const color_target = gpu.ColorTargetState{
-        .format = window_data.swap_chain_format,
+        .format = gctx.swap_chain_format,
         .blend = &blend,
         .write_mask = .all,
     };
@@ -137,26 +92,25 @@ pub fn main() !void {
             .strip_index_format = .none,
         },
     };
-    const pipeline = setup.device.createRenderPipeline(&pipeline_descriptor);
+    const pipeline = gctx.device.createRenderPipeline(&pipeline_descriptor);
 
     vs_module.release();
     fs_module.release();
 
     // Reconfigure the swap chain with the new framebuffer width/height, otherwise e.g. the Vulkan
     // device would be lost after a resize.
-    setup.window.setFramebufferSizeCallback((struct {
-        fn callback(window: glfw.Window, width: u32, height: u32) void {
-            const pl = window.getUserPointer(WindowData);
-            pl.?.target_desc.width = width;
-            pl.?.target_desc.height = height;
-        }
-    }).callback);
+    //window.setFramebufferSizeCallback((struct {
+    //    fn callback(win: glfw.Window, width: u32, height: u32) void {
+    //        const gctx = win.getUserPointer(zgpu.GraphicsContext);
+    //        gctx.?.swap_chain_descriptor_target.width = width;
+    //        gctx.?.swap_chain_descriptor_target.height = height;
+    //    }
+    //}).callback);
 
-    const queue = setup.device.getQueue();
-    while (!setup.window.shouldClose()) {
+    const queue = gctx.device.getQueue();
+    while (!window.shouldClose()) {
         try frame(.{
-            .window = setup.window,
-            .device = setup.device,
+            .gctx = &gctx,
             .pipeline = pipeline,
             .queue = queue,
         });
@@ -164,38 +118,17 @@ pub fn main() !void {
     }
 }
 
-const WindowData = struct {
-    surface: ?gpu.Surface,
-    swap_chain: ?gpu.SwapChain,
-    swap_chain_format: gpu.Texture.Format,
-    current_desc: gpu.SwapChain.Descriptor,
-    target_desc: gpu.SwapChain.Descriptor,
-};
-
 const FrameParams = struct {
-    window: glfw.Window,
-    device: gpu.Device,
+    gctx: *zgpu.GraphicsContext,
     pipeline: gpu.RenderPipeline,
     queue: gpu.Queue,
 };
 
 fn frame(params: FrameParams) !void {
     try glfw.pollEvents();
-    const pl = params.window.getUserPointer(WindowData).?;
-    if (pl.swap_chain == null or !pl.current_desc.equal(&pl.target_desc)) {
-        const use_legacy_api = pl.surface == null;
-        if (!use_legacy_api) {
-            pl.swap_chain = params.device.nativeCreateSwapChain(pl.surface, &pl.target_desc);
-        } else pl.swap_chain.?.configure(
-            pl.swap_chain_format,
-            .render_attachment,
-            pl.target_desc.width,
-            pl.target_desc.height,
-        );
-        pl.current_desc = pl.target_desc;
-    }
+    params.gctx.update();
 
-    const back_buffer_view = pl.swap_chain.?.getCurrentTextureView();
+    const back_buffer_view = params.gctx.swap_chain.?.getCurrentTextureView();
     const color_attachment = gpu.RenderPassColorAttachment{
         .view = back_buffer_view,
         .resolve_target = null,
@@ -204,7 +137,7 @@ fn frame(params: FrameParams) !void {
         .store_op = .store,
     };
 
-    const encoder = params.device.createCommandEncoder(null);
+    const encoder = params.gctx.device.createCommandEncoder(null);
     const render_pass_info = gpu.RenderPassEncoder.Descriptor{
         .color_attachments = &.{color_attachment},
         .depth_stencil_attachment = null,
@@ -220,6 +153,6 @@ fn frame(params: FrameParams) !void {
 
     params.queue.submit(&.{command});
     command.release();
-    pl.swap_chain.?.present();
+    params.gctx.swap_chain.?.present();
     back_buffer_view.release();
 }
