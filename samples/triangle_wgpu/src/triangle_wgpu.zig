e3e66de090cb46972df92ee1e46d379da58e0760
Michal Ziulek <michalziulek@gmail.com>
2022-04-09 22:36:29 +0200

    triangle_wgpu: Added vertex buffer.

--- samples/triangle_wgpu/src/triangle_wgpu.zig
+++ samples/triangle_wgpu/src/triangle_wgpu.zig
@@ -9,21 +9,12 @@ const wgsl_vs =
 \\      @location(0) color : vec3<f32>;
 \\  }
 \\  @stage(vertex) fn main(
-\\      @builtin(vertex_index) VertexIndex : u32
+\\      @location(0) position : vec3<f32>,
+\\      @location(1) color : vec3<f32>
 \\  ) -> VertexOut {
-\\     let positions = array<vec2<f32>, 3>(
-\\         vec2(0.0, 0.5),
-\\         vec2(-0.5, -0.5),
-\\         vec2(0.5, -0.5)
-\\     );
-\\     let colors = array<vec3<f32>, 3>(
-\\         vec3(1.0, 0.0, 0.0),
-\\         vec3(0.0, 1.0, 0.0),
-\\         vec3(0.0, 0.0, 1.0),
-\\     );
 \\     var output : VertexOut;
-\\     output.position_clip = vec4(positions[VertexIndex], 0.0, 1.0);
-\\     output.color = colors[VertexIndex];
+\\     output.position_clip = vec4(position, 1.0);
+\\     output.color = color;
 \\     return output;
 \\ }
 ;
@@ -36,9 +27,15 @@ const wgsl_fs =
 // zig fmt: on
 ;
 
+const Vertex = struct {
+    position: [3]f32,
+    color: [3]f32,
+};
+
 const DemoState = struct {
     gctx: *zgpu.GraphicsContext,
     pipeline: zgpu.RenderPipeline,
+    vertex_buffer: zgpu.Buffer,
 };
 
 fn init(allocator: std.mem.Allocator, window: glfw.Window) DemoState {
@@ -63,18 +60,29 @@ fn init(allocator: std.mem.Allocator, window: glfw.Window) DemoState {
         .module = fs_module,
         .entry_point = "main",
         .targets = &.{color_target},
-        .constants = null,
+    };
+
+    const vertex_attributes = [_]zgpu.VertexAttribute{
+        zgpu.VertexAttribute{ .format = .float32x3, .offset = 0, .shader_location = 0 },
+        zgpu.VertexAttribute{ .format = .float32x3, .offset = @sizeOf([3]f32), .shader_location = 1 },
+    };
+    const vertex_buffer_layout = zgpu.VertexBufferLayout{
+        .array_stride = @sizeOf(Vertex),
+        .step_mode = .vertex,
+        .attribute_count = vertex_attributes.len,
+        .attributes = &vertex_attributes,
+    };
+    const vertex = zgpu.VertexState{
+        .module = vs_module,
+        .entry_point = "main",
+        .buffers = &.{vertex_buffer_layout},
     };
 
     const pipeline_descriptor = zgpu.RenderPipeline.Descriptor{
         .fragment = &fragment,
         .layout = null,
         .depth_stencil = null,
-        .vertex = .{
-            .module = vs_module,
-            .entry_point = "main",
-            .buffers = null,
-        },
+        .vertex = vertex,
         .multisample = .{
             .count = 1,
             .mask = 0xffff_ffff,
@@ -89,14 +97,28 @@ fn init(allocator: std.mem.Allocator, window: glfw.Window) DemoState {
     };
     const pipeline = gctx.device.createRenderPipeline(&pipeline_descriptor);
 
+    const vertex_buffer = gctx.device.createBuffer(&.{
+        .usage = .{ .copy_dst = true, .vertex = true },
+        .size = 3 * @sizeOf(Vertex),
+        .mapped_at_creation = false,
+    });
+    const vertex_data = [_]Vertex{
+        .{ .position = [3]f32{ 0.0, 0.5, 0.0 }, .color = [3]f32{ 1.0, 0.0, 0.0 } },
+        .{ .position = [3]f32{ -0.5, -0.5, 0.0 }, .color = [3]f32{ 0.0, 1.0, 0.0 } },
+        .{ .position = [3]f32{ 0.5, -0.5, 0.0 }, .color = [3]f32{ 0.0, 0.0, 1.0 } },
+    };
+    gctx.device_queue.writeBuffer(vertex_buffer, 0, Vertex, vertex_data[0..]);
+
     return .{
         .gctx = gctx,
         .pipeline = pipeline,
+        .vertex_buffer = vertex_buffer,
     };
 }
 
 fn deinit(allocator: std.mem.Allocator, demo: *DemoState) void {
     demo.pipeline.release();
+    demo.vertex_buffer.release();
     allocator.destroy(demo.gctx);
     demo.* = undefined;
 }
@@ -127,6 +149,7 @@ fn draw(demo: *DemoState) void {
             defer pass.release();
 
             pass.setPipeline(demo.pipeline);
+            pass.setVertexBuffer(0, demo.vertex_buffer, 0, 3 * @sizeOf([6]f32));
             pass.draw(3, 1, 0, 0);
             pass.end();
         }
