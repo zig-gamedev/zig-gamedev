49b1632f2b67fd1135a98e851065c95244080c6e
Michal Ziulek <michalziulek@gmail.com>
2022-04-12 11:51:49 +0200

    triangle_wgpu: Fixes.

--- libs/common/src/common.zig
+++ libs/common/src/common.zig
@@ -19,6 +19,57 @@ pub const c = @cImport({
     @cInclude("stb_image.h");
 });
 
+pub const FrameStats = struct {
+    time: f64,
+    delta_time: f32,
+    fps: f32,
+    average_cpu_time: f32,
+    timer: std.time.Timer,
+    previous_time_ns: u64,
+    fps_refresh_time_ns: u64,
+    frame_counter: u64,
+
+    pub fn init() FrameStats {
+        return .{
+            .time = 0.0,
+            .delta_time = 0.0,
+            .fps = 0.0,
+            .average_cpu_time = 0.0,
+            .timer = std.time.Timer.start() catch unreachable,
+            .previous_time_ns = 0,
+            .fps_refresh_time_ns = 0,
+            .frame_counter = 0,
+        };
+    }
+
+    pub fn update(self: *FrameStats, window: w32.HWND, window_name: []const u8) void {
+        const now_ns = self.timer.read();
+        self.time = @intToFloat(f64, now_ns) / std.time.ns_per_s;
+        self.delta_time = @intToFloat(f32, now_ns - self.previous_time_ns) / std.time.ns_per_s;
+        self.previous_time_ns = now_ns;
+
+        if ((now_ns - self.fps_refresh_time_ns) >= std.time.ns_per_s) {
+            const t = @intToFloat(f64, now_ns - self.fps_refresh_time_ns) / std.time.ns_per_s;
+            const fps = @intToFloat(f64, self.frame_counter) / t;
+            const ms = (1.0 / fps) * 1000.0;
+
+            self.fps = @floatCast(f32, fps);
+            self.average_cpu_time = @floatCast(f32, ms);
+            self.fps_refresh_time_ns = now_ns;
+            self.frame_counter = 0;
+
+            var buffer = [_]u8{0} ** 128;
+            const text = std.fmt.bufPrint(
+                buffer[0..],
+                "FPS: {d:.1}  CPU time: {d:.3} ms | {s}",
+                .{ self.fps, self.average_cpu_time, window_name },
+            ) catch unreachable;
+            _ = w32.SetWindowTextA(window, @ptrCast([*:0]const u8, text.ptr));
+        }
+        self.frame_counter += 1;
+    }
+};
+
 fn processWindowMessage(
     window: w32.HWND,
     message: w32.UINT,
