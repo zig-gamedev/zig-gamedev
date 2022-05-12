8bb9fd92f841e106926980671224bf8265afaf82
Michal Ziulek <michalziulek@gmail.com>
2022-05-12 20:04:07 +0200

    zgpu: Added some documentation (wip).

--- libs/zgpu/src/zgpu.zig
+++ libs/zgpu/src/zgpu.zig
@@ -1,3 +1,47 @@
+//  zgpu v0.1
+//
+//  1. Init
+//
+//      const gctx = try zgpu.GraphicsContext.init(allocator, window);
+//
+//      // When you are done:
+//      gctx.deinit(allocator);
+//
+//  2. Uniforms
+//
+//      * Implemented as a uniform buffer pool
+//      * Easy to use
+//      * Efficient - only one copy operation per frame
+//
+//      struct DrawUniforms = extern struct {
+//          object_to_world: zm.Mat,
+//      };
+//      const mem = gctx.uniformsAllocate(DrawUniforms, 1);
+//      mem.slice[0] = .{ .object_to_world = zm.transpose(zm.translation(...)) };
+//      pass.setBindGroup(0, bind_group, &.{mem.offset});
+//      pass.drawIndexed(...);
+//
+//      // When you are done encoding all commands for a frame:
+//      gctx.submit(...); // Injects one copy operation to transfer *all* allocated uniforms
+//
+//  3. Resource pools
+//
+//      * Every GPU resource is identified by 32-bit integer handle
+//      * All resources are stored in one system
+//      * We keep basic info about each resource (size of the buffer, format of the texture, etc.)
+//      * You can always check if resource is valid (very useful for async operations)
+//      * System keeps basic info about resource dependencies, for example, TextureViewHandle knows its
+//        parent texture and becomes invalid when parent texture becomes invalid
+//
+//      const buffer_handle = gctx.createBuffer(...);
+//      if (gctx.isResourceValid(buffer_handle)) {
+//          const buffer = gctx.lookupResource(buffer_handle).?;  // Returns gpu.Buffer
+//          const buffer_info = gctx.lookupResourceInfo(buffer_handle).?; // Returns zgpu.BufferInfo
+//          std.debug.print("Buffer size is: {d}, .{buffer_info.size});
+//      }
+//      // If you want to destroy a resource before shutting down graphics context:
+//      gctx.destroyResource(buffer_handle);
+//
 const std = @import("std");
 const math = std.math;
 const assert = std.debug.assert;
