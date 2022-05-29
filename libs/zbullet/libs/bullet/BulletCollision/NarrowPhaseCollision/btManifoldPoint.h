dba18bbfef0d842334a5da1b6dfc4eda91188fbd
Michal Ziulek <michalziulek@gmail.com>
2022-05-29 11:25:01 +0200

    zbullet: Updated Bullet sources to the latest master version.

--- libs/zbullet/libs/bullet/BulletCollision/NarrowPhaseCollision/btManifoldPoint.h
+++ libs/zbullet/libs/bullet/BulletCollision/NarrowPhaseCollision/btManifoldPoint.h
@@ -71,9 +71,9 @@ public:
 					const btVector3& normal,
 					btScalar distance) : m_localPointA(pointA),
 										 m_localPointB(pointB),
+										 m_positionWorldOnB(0,0,0),
+										 m_positionWorldOnA(0,0,0),
 										 m_normalWorldOnB(normal),
-                     m_positionWorldOnB(0,0,0),
-                     m_positionWorldOnA(0,0,0),
 										 m_distance1(distance),
 										 m_combinedFriction(btScalar(0.)),
 										 m_combinedRollingFriction(btScalar(0.)),
@@ -95,8 +95,8 @@ public:
 										 m_contactERP(0.f),
 										 m_frictionCFM(0.f),
 										 m_lifeTime(0),
-            m_lateralFrictionDir1(0,0,0),
-            m_lateralFrictionDir2(0,0,0)
+										 m_lateralFrictionDir1(0,0,0),
+										 m_lateralFrictionDir2(0,0,0)
 	{
 	}
 
