11d50f2ccf862e5de2505e518e4f0144704bcdd8
Michal Ziulek <michalziulek@gmail.com>
2022-06-22 09:48:30 +0200

    zaudio: More bindings..

--- libs/zaudio/src/zaudio.zig
+++ libs/zaudio/src/zaudio.zig
@@ -19,6 +19,42 @@ pub const Engine = struct {
         c.ma_engine_uninit(engine.handle);
         allocator.destroy(engine.handle);
     }
+
+    pub fn readPcmFrames(engine: Engine, comptime T: type, frames: []T, frames_read: ?*u64) Error!void {
+        try checkResult(c.ma_engine_read_pcm_frames(engine.handle, frames.ptr, frames.len, frames_read));
+    }
+
+    pub fn getTime(engine: Engine) u64 {
+        return c.ma_engine_get_time(engine.handle);
+    }
+
+    pub fn setTime(engine: Engine, global_time: u64) Error!void {
+        try checkResult(c.ma_engine_set_time(engine.handle, global_time));
+    }
+
+    pub fn getChannels(engine: Engine) u32 {
+        return c.ma_engine_get_channels(engine.handle);
+    }
+
+    pub fn getSampleRate(engine: Engine) u32 {
+        return c.ma_engine_get_sample_rate(engine.handle);
+    }
+
+    pub fn start(engine: Engine) Error!void {
+        try checkResult(c.ma_engine_start(engine.handle));
+    }
+
+    pub fn stop(engine: Engine) Error!void {
+        try checkResult(c.ma_engine_stop(engine.handle));
+    }
+
+    pub fn setVolume(engine: Engine, volume: f32) Error!void {
+        try checkResult(c.ma_engine_set_volume(engine.handle, volume));
+    }
+
+    pub fn setGainDb(engine: Engine, gain_db: f32) Error!void {
+        try checkResult(c.ma_engine_set_gain_db(engine.handle, gain_db));
+    }
 };
 
 pub const Error = error{
@@ -34,7 +70,20 @@ fn checkResult(result: c.ma_result) Error!void {
         return error.GenericError;
 }
 
-test "zaudio.init" {
+test "zaudio.engine.basic" {
     const engine = try Engine.init(std.testing.allocator, null);
     defer engine.deinit(std.testing.allocator);
+
+    var frames: [2]f32 = undefined;
+    try engine.readPcmFrames(f32, frames[0..], null);
+
+    try engine.setTime(engine.getTime());
+
+    std.debug.print("Channels: {}, SampleRate: {}\n", .{ engine.getChannels(), engine.getSampleRate() });
+
+    try engine.start();
+    try engine.stop();
+    try engine.start();
+    try engine.setVolume(1.0);
+    try engine.setGainDb(1.0);
 }
