01c5d57ab9263396cce8c7e9c10e2d8d48087acc
Michal Ziulek <michalziulek@gmail.com>
2022-06-23 13:57:37 +0200

    zaudio: Added Sound struct.

--- libs/zaudio/src/zaudio.zig
+++ libs/zaudio/src/zaudio.zig
@@ -37,15 +37,16 @@ pub const Positioning = enum(c_int) {
 };
 
 pub const EngineConfig = c.ma_engine_config;
+pub const SoundConfig = c.ma_sound_config;
 
 pub const Engine = struct {
     handle: *c.ma_engine,
 
-    pub fn init(allocator: std.mem.Allocator, config: ?*const EngineConfig) Error!Engine {
+    pub fn init(allocator: std.mem.Allocator, config: ?EngineConfig) Error!Engine {
         var handle = allocator.create(c.ma_engine) catch return error.OutOfMemory;
         errdefer allocator.destroy(handle);
 
-        try checkResult(c.ma_engine_init(config, handle));
+        try checkResult(c.ma_engine_init(if (config != null) &config.? else null, handle));
 
         return Engine{ .handle = handle };
     }
@@ -158,6 +159,281 @@ pub const Engine = struct {
         outer_gain: ?*f32,
     ) void {
         c.ma_engine_listener_get_cone(engine.handle, index, inner_radians, outer_radians, outer_gain);
+    }
+};
+
+pub const Sound = struct {
+    handle: *c.ma_sound,
+
+    pub fn initFromFile(
+        allocator: std.mem.Allocator,
+        engine: Engine,
+        filepath: [:0]const u8,
+        flags: SoundFlags,
+        sgroup: ?SoundGroup,
+        done_fence: ?Fence,
+    ) Error!Sound {
+        var handle = allocator.create(c.ma_sound) catch return error.OutOfMemory;
+        errdefer allocator.destroy(handle);
+
+        try checkResult(c.ma_sound_init_from_file(
+            engine.handle,
+            filepath.ptr,
+            @bitCast(u32, flags),
+            if (sgroup) |g| g.handle else null,
+            if (done_fence) |f| f.handle else null,
+            handle,
+        ));
+
+        return Sound{ .handle = handle };
+    }
+
+    pub fn initFromDataSource(
+        allocator: std.mem.Allocator,
+        engine: Engine,
+        data_source: DataSource,
+        flags: SoundFlags,
+        sgroup: ?SoundGroup,
+    ) Error!Sound {
+        var handle = allocator.create(c.ma_sound) catch return error.OutOfMemory;
+        errdefer allocator.destroy(handle);
+
+        try checkResult(c.ma_sound_init_from_data_source(
+            engine.handle,
+            data_source.handle,
+            @bitCast(u32, flags),
+            if (sgroup) |g| g.handle else null,
+            handle,
+        ));
+
+        return Sound{ .handle = handle };
+    }
+
+    pub fn initCopy(
+        allocator: std.mem.Allocator,
+        engine: Engine,
+        existing_sound: Sound,
+        flags: SoundFlags,
+        sgroup: ?SoundGroup,
+    ) Error!Sound {
+        var handle = allocator.create(c.ma_sound) catch return error.OutOfMemory;
+        errdefer allocator.destroy(handle);
+
+        try checkResult(c.ma_sound_init_copy(
+            engine.handle,
+            existing_sound.handle,
+            @bitCast(u32, flags),
+            if (sgroup) |g| g.handle else null,
+            handle,
+        ));
+
+        return Sound{ .handle = handle };
+    }
+
+    pub fn initWithConfig(
+        allocator: std.mem.Allocator,
+        engine: Engine,
+        config: SoundConfig,
+    ) Error!Sound {
+        var handle = allocator.create(c.ma_sound) catch return error.OutOfMemory;
+        errdefer allocator.destroy(handle);
+
+        try checkResult(c.ma_sound_init_ex(engine.handle, &config, handle));
+
+        return Sound{ .handle = handle };
+    }
+
+    pub fn getEngine(sound: Sound) Engine {
+        return Engine{ .handle = c.ma_sound_get_engine(sound.handle) };
+    }
+
+    pub fn getDataSource(sound: Sound) ?DataSource {
+        const ds = c.ma_sound_get_data_source(sound.handle);
+        if (ds != null) return DataSource{ .handle = ds };
+        return null;
+    }
+
+    pub fn start(sound: Sound) Error!void {
+        try checkResult(c.ma_sound_start(sound.handle));
+    }
+    pub fn stop(sound: Sound) Error!void {
+        try checkResult(c.ma_sound_stop(sound.handle));
+    }
+
+    pub fn setVolume(sound: Sound, volume: f32) void {
+        c.ma_sound_set_volume(sound.handle, volume);
+    }
+    pub fn getVolume(sound: Sound) f32 {
+        return c.ma_sound_get_volume(sound.handle);
+    }
+
+    pub fn setPan(sound: Sound, pan: f32) void {
+        c.ma_sound_set_pan(sound.handle, pan);
+    }
+    pub fn getPan(sound: Sound) f32 {
+        return c.ma_sound_get_pan(sound.handle);
+    }
+
+    pub fn setPanMode(sound: Sound, pan_mode: PanMode) void {
+        c.ma_sound_set_pan_mode(sound.handle, pan_mode);
+    }
+    pub fn getPanMode(sound: Sound) PanMode {
+        return c.ma_sound_get_pan_mode(sound.handle);
+    }
+
+    pub fn setPitch(sound: Sound, pitch: f32) void {
+        c.ma_sound_set_pitch(sound.handle, pitch);
+    }
+    pub fn getPitch(sound: Sound) f32 {
+        return c.ma_sound_get_pitch(sound.handle);
+    }
+
+    pub fn setSpatializationEnabled(sound: Sound, enabled: bool) void {
+        c.ma_sound_set_spatialization_enabled(sound.handle, if (enabled) c.MA_TRUE else c.MA_FALSE);
+    }
+    pub fn isSpatializationEnabled(sound: Sound) bool {
+        return c.ma_sound_is_spatialization_enabled(sound.handle) == c.MA_TRUE;
+    }
+
+    pub fn setPinnedListenerIndex(sound: Sound, index: u32) void {
+        c.ma_sound_set_pinned_listener_index(sound.handle, index);
+    }
+    pub fn getPinnedListenerIndex(sound: Sound) u32 {
+        return c.ma_sound_get_pinned_listener_index(sound.handle);
+    }
+    pub fn getListenerIndex(sound: Sound) u32 {
+        return c.ma_sound_get_listener_index(sound.handle);
+    }
+
+    pub fn getDirectionToListener(sound: Sound) [3]f32 {
+        const v = c.ma_sound_get_direction_to_listener(sound.handle);
+        return .{ v.x, v.y, v.z };
+    }
+
+    pub fn setPosition(sound: Sound, v: [3]f32) void {
+        c.ma_sound_set_position(sound.handle, v[0], v[1], v[2]);
+    }
+    pub fn getPosition(sound: Sound) [3]f32 {
+        const v = c.ma_sound_get_position(sound.handle);
+        return .{ v.x, v.y, v.z };
+    }
+
+    pub fn setDirection(sound: Sound, v: [3]f32) void {
+        c.ma_sound_set_direction(sound.handle, v[0], v[1], v[2]);
+    }
+    pub fn getDirection(sound: Sound) [3]f32 {
+        const v = c.ma_sound_get_direction(sound.handle);
+        return .{ v.x, v.y, v.z };
+    }
+
+    pub fn setVelocity(sound: Sound, v: [3]f32) void {
+        c.ma_sound_set_velocity(sound.handle, v[0], v[1], v[2]);
+    }
+    pub fn getVelocity(sound: Sound) [3]f32 {
+        const v = c.ma_sound_get_velocity(sound.handle);
+        return .{ v.x, v.y, v.z };
+    }
+
+    pub fn setAttenuationModel(sound: Sound, model: AttenuationModel) void {
+        c.ma_sound_set_attenuation_model(sound.handle, model);
+    }
+    pub fn getAttenuationModel(sound: Sound) AttenuationModel {
+        return c.ma_sound_get_attenuation_model(sound.handle);
+    }
+
+    pub fn setPositioning(sound: Sound, pos: Positioning) void {
+        c.ma_sound_set_positioning(sound.handle, pos);
+    }
+    pub fn getPositioning(sound: Sound) Positioning {
+        return c.ma_sound_get_positioning(sound.handle);
+    }
+
+    pub fn setRolloff(sound: Sound, rolloff: f32) void {
+        c.ma_sound_set_rolloff(sound.handle, rolloff);
+    }
+    pub fn getRolloff(sound: Sound) f32 {
+        return c.ma_sound_get_rolloff(sound.handle);
+    }
+
+    pub fn setMinGain(sound: Sound, min_gain: f32) void {
+        c.ma_sound_set_min_gain(sound.handle, min_gain);
+    }
+    pub fn getMinGain(sound: Sound) f32 {
+        return c.ma_sound_get_min_gain(sound.handle);
+    }
+
+    pub fn setMaxGain(sound: Sound, max_gain: f32) void {
+        c.ma_sound_set_max_gain(sound.handle, max_gain);
+    }
+    pub fn getMaxGain(sound: Sound) f32 {
+        return c.ma_sound_get_max_gain(sound.handle);
+    }
+
+    pub fn setMinDistance(sound: Sound, min_distance: f32) void {
+        c.ma_sound_set_min_distance(sound.handle, min_distance);
+    }
+    pub fn getMinDistance(sound: Sound) f32 {
+        return c.ma_sound_get_min_distance(sound.handle);
+    }
+
+    pub fn setMaxDistance(sound: Sound, max_distance: f32) void {
+        c.ma_sound_set_max_distance(sound.handle, max_distance);
+    }
+    pub fn getMaxDistance(sound: Sound) f32 {
+        return c.ma_sound_get_max_distance(sound.handle);
+    }
+
+    pub fn setCone(sound: Sound, inner_radians: f32, outer_radians: f32, outer_gain: f32) void {
+        c.ma_sound_set_cone(sound.handle, inner_radians, outer_radians, outer_gain);
+    }
+    pub fn getCone(sound: Sound, inner_radians: ?*f32, outer_radians: ?*f32, outer_gain: ?*f32) void {
+        c.ma_sound_get_cone(sound.handle, inner_radians, outer_radians, outer_gain);
+    }
+
+    pub fn setDopplerFactor(sound: Sound, factor: f32) void {
+        c.ma_sound_set_doppler_factor(sound.handle, factor);
+    }
+    pub fn getDopplerFactor(sound: Sound) f32 {
+        return c.ma_sound_get_doppler_factor(sound.handle);
+    }
+
+    pub fn setDirectionalAttenuationFactor(sound: Sound, factor: f32) void {
+        c.ma_sound_set_directional_attenuation_factor(sound.handle, factor);
+    }
+    pub fn getDirectionalAttenuationFactor(sound: Sound) f32 {
+        return c.ma_sound_get_directional_attenuation_factor(sound.handle);
+    }
+
+    pub fn setFadeInPcmFrames(sound: Sound, volume_begin: f32, volume_end: f32, len_in_frames: u64) void {
+        c.ma_sound_set_fade_in_pcm_frames(sound.handle, volume_begin, volume_end, len_in_frames);
+    }
+    pub fn setFadeInMilliseconds(sound: Sound, volume_begin: f32, volume_end: f32, len_in_ms: u64) void {
+        c.ma_sound_set_fade_in_milliseconds(sound.handle, volume_begin, volume_end, len_in_ms);
+    }
+    pub fn getCurrentFadeVolume(sound: Sound) f32 {
+        return c.ma_sound_get_current_fade_volume(sound.handle);
+    }
+
+    pub fn setStartTimeInPcmFrames(sound: Sound, abs_global_time_in_frames: u64) void {
+        c.ma_sound_set_start_time_in_pcm_frames(sound.handle, abs_global_time_in_frames);
+    }
+    pub fn setStartTimeInMilliseconds(sound: Sound, abs_global_time_in_ms: u64) void {
+        c.ma_sound_set_start_time_in_milliseconds(sound.handle, abs_global_time_in_ms);
+    }
+
+    pub fn setStopTimeInPcmFrames(sound: Sound, abs_global_time_in_frames: u64) void {
+        c.ma_sound_set_stop_time_in_pcm_frames(sound.handle, abs_global_time_in_frames);
+    }
+    pub fn setStopTimeInMilliseconds(sound: Sound, abs_global_time_in_ms: u64) void {
+        c.ma_sound_set_stop_time_in_milliseconds(sound.handle, abs_global_time_in_ms);
+    }
+
+    pub fn isPlaying(sound: Sound) bool {
+        return c.ma_sound_is_playing(sound.handle) == c.MA_TRUE;
+    }
+
+    pub fn getTimeInPcmFrames(sound: Sound) u64 {
+        return c.ma_sound_get_time_in_pcm_frames(sound.handle);
     }
 };
 
@@ -404,6 +680,12 @@ pub const Fence = struct {
     pub fn wait(fence: Fence) Error!void {
         try checkResult(c.ma_fence_wait(fence.handle));
     }
+};
+
+pub const DataSource = struct {
+    handle: *c.ma_data_source,
+
+    // TODO: Add methods.
 };
 
 pub const Error = error{
