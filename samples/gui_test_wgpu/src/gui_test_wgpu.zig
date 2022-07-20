e4654fb65a55ac13596c9a1322d8d70137a8a64f
Michal Ziulek <michalziulek@gmail.com>
2022-07-20 13:40:55 +0200

    zgui + demo: Minor tweak.

--- samples/gui_test_wgpu/src/gui_test_wgpu.zig
+++ samples/gui_test_wgpu/src/gui_test_wgpu.zig
@@ -57,7 +57,7 @@ fn update(demo: *DemoState) !void {
     zgui.dummy(.{ .w = -1.0, .h = 20.0 });
     zgui.textUnformattedColored(.{ 0, 0.8, 0, 1 }, "zgui -");
     zgui.sameLine(.{});
-    zgui.textWrapped("Zig bindings for 'dear imgui' library. Easy to use API with defualt arguments, named parameters and Zig style text formatting.", .{});
+    zgui.textWrapped("Zig bindings for 'dear imgui' library. Easy to use, hand-crafted API with defualt arguments, named parameters and Zig style text formatting.", .{});
     zgui.dummy(.{ .w = -1.0, .h = 20.0 });
     zgui.separator();
 
