64b651eeb9ed2a2c9ac92e8165f28be8dc225ca3
Connor Rowland <mail@connorrowland.me>
2022-08-24 01:28:32 -0700

    Another test

--- libs/zgpu/build.zig
+++ libs/zgpu/build.zig
@@ -58,7 +58,7 @@ pub fn link(exe: *std.build.LibExeObjStep, bos: BuildOptionsStep) void {
         exe.addCSourceFile(thisDir() ++ "/libs/imgui/implot_demo.cpp", &.{""});
         exe.addCSourceFile(thisDir() ++ "/libs/imgui/implot.cpp", &.{""});
         exe.addCSourceFile(thisDir() ++ "/libs/imgui/implot.h", &.{""});
-        exe.addCSourceFile(thisDir() ++ "/libs/imgui/implot_internal.h", &.{""});
+        //exe.addCSourceFile(thisDir() ++ "/libs/imgui/implot_internal.h", &.{""});
         exe.addCSourceFile(thisDir() ++ "/libs/imgui/implot_items.cpp", &.{""});
     }
 
