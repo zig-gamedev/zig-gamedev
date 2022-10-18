520b81329e606ea20fd923294cdebaf008c16317
Michal Ziulek <michalziulek@gmail.com>
2022-10-18 13:55:31 +0200

    zgui: Added 'zgui.version' const.

--- libs/zgui/src/main.zig
+++ libs/zgui/src/main.zig
@@ -1,10 +1,11 @@
 //--------------------------------------------------------------------------------------------------
 //
-// zgui v0.9.1
 // Zig bindings for 'dear imgui' library. Easy to use, hand-crafted API with default arguments,
 // named parameters and Zig style text formatting.
 //
 //--------------------------------------------------------------------------------------------------
+pub const version = @import("std").SemanticVersion{ .major = 0, .minor = 9, .patch = 1 };
+
 pub usingnamespace @import("gui.zig");
 pub const plot = @import("plot.zig");
 pub const backend = @import("backend_glfw_wgpu.zig");
