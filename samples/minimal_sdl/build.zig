b842f6446bb6c21ff2f4b2aef219f95e2aeeac96
Michal Ziulek <michalziulek@gmail.com>
2023-02-20 22:04:03 +0100

    zsdl: Smaller binary distribution on macOS.

--- samples/minimal_sdl/build.zig
+++ samples/minimal_sdl/build.zig
@@ -43,12 +43,12 @@ pub fn build(b: *std.Build, options: Options) *std.Build.CompileStep {
             exe.linkFramework("SDL2");
             exe.addRPath("@executable_path/Frameworks");
 
-            const install_dir_step = b.addInstallDirectory(.{
-                .source_dir = thisDir() ++ "/../../libs/zsdl/libs/macos/Frameworks/SDL2.framework",
-                .install_dir = .{ .custom = "" },
-                .install_subdir = "bin/Frameworks/SDL2.framework",
-            });
-            exe.step.dependOn(&install_dir_step.step);
+            exe.step.dependOn(
+                &exe.builder.addInstallFile(
+                    .{ .path = thisDir() ++ "/../../libs/zsdl/libs/macos/Frameworks/SDL2.framework/SDL2" },
+                    "bin/Frameworks/SDL2.framework/Versions/A/SDL2",
+                ).step,
+            );
         },
         else => unreachable,
     }
