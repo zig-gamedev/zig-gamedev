42a799548d7fc756a68ee28df0ebb0c78bf33b13
Michal Ziulek <michalziulek@gmail.com>
2023-03-23 18:07:51 +0100

    experiments(x0001): Fractal flames.

--- experiments/genart/src/x0001.zig
+++ experiments/genart/src/x0001.zig
@@ -5,36 +5,97 @@ const gl = @import("zopengl");
 const xcommon = @import("xcommon");
 
 pub const name = "generative art experiment: x0001";
-pub const viewport_width = 1600;
-pub const viewport_height = 1600;
+pub const viewport_width = 1024 * 2;
+pub const viewport_height = 1024 * 2;
 
-var rot: f32 = 0.0;
+const Vec2 = [2]f32;
+const bounds: f32 = 3.0;
+
+var prng = std.rand.DefaultPrng.init(123);
+var random = prng.random();
+var pass: u32 = 0;
+var y: f32 = -bounds;
 
 pub fn draw() void {
-    gl.loadIdentity();
-    gl.rotatef(rot, 0, 0, 1);
-    rot += 0.1;
-    rot = @mod(rot, 360.0);
+    if (y <= bounds and pass <= 3) {
+        gl.begin(gl.POINTS);
+        const step: f32 = 0.001;
+        var row: u32 = 0;
+        while (row < 4) : (row += 1) {
+            var x: f32 = -bounds;
+            while (x <= bounds) : (x += step) {
+                var v = Vec2{ x, y };
 
-    gl.begin(gl.LINE_LOOP);
-    gl.color4f(1.0, 0.0, 0.0, 0.01);
-    gl.vertex2f(-50.0, -50.0);
-    gl.color4f(0.0, 1.0, 0.0, 0.01);
-    gl.vertex2f(50.0, -50.0);
-    gl.color4f(0.0, 0.0, 1.0, 0.01);
-    gl.vertex2f(0.0, 50.0);
-    gl.end();
+                v = pdj(v, 2.0);
+
+                if (pass == 0) v = julia(v, 2.5, random.float(f32));
+                if (pass == 1) v = hyperbolic(v, 1.0);
+
+                //v = sinusoidal(v, 2.2);
+
+                if (pass == 0) gl.color4f(0.002, 0.0, 0.0, 0.0);
+                if (pass == 1) gl.color4f(0.0, 0.001, 0.0, 0.0);
+                if (pass == 2) gl.color4f(0.0, 0.0005, 0.001, 0.0);
+
+                const xoff = random.floatNorm(f32) * 0.0125;
+                const yoff = random.floatNorm(f32) * 0.0125;
+                gl.vertex2f(v[0] + xoff, v[1] + yoff);
+            }
+            y += step;
+        }
+        gl.end();
+    }
+
+    if (y >= bounds) {
+        y = -bounds;
+        pass += 1;
+    }
 }
 
 pub fn init() !void {
     try sdl.gl.setSwapInterval(1);
 
-    gl.lineWidth(15.0);
-
+    gl.clearBufferfv(gl.COLOR, 0, &[_]f32{ 0.0, 0.0, 0.0, 1.0 });
+    gl.pointSize(1.0);
     gl.enable(gl.BLEND);
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-
-    gl.clearBufferfv(gl.COLOR, 0, &[_]f32{ 0.05, 0.05, 0.05, 1.0 });
+    gl.blendFunc(gl.ONE, gl.ONE);
+    gl.blendEquation(gl.FUNC_ADD);
     gl.matrixLoadIdentityEXT(gl.PROJECTION);
-    gl.matrixOrthoEXT(gl.PROJECTION, -100.0, 100.0, -100.0, 100.0, -1.0, 1.0);
+    gl.matrixOrthoEXT(gl.PROJECTION, -3.0, 3.0, -3.0, 3.0, -1.0, 1.0);
+}
+
+fn sinusoidal(v: Vec2, scale: f32) Vec2 {
+    return .{ scale * math.sin(v[0]), scale * math.sin(v[1]) };
+}
+
+fn hyperbolic(v: Vec2, scale: f32) Vec2 {
+    const r = @sqrt(v[0] * v[0] + v[1] * v[1]) + 0.0001;
+    const theta = math.atan2(f32, v[0], v[1]);
+    const xx = scale * math.sin(theta) / r;
+    const yy = scale * math.cos(theta) * r;
+    return .{ xx, yy };
+}
+
+fn pdj(v: Vec2, scale: f32) Vec2 {
+    const pdj_a = 0.1;
+    const pdj_b = 1.9;
+    const pdj_c = -0.8;
+    const pdj_d = -1.2;
+    //const pdj_a = 1.0111;
+    //const pdj_b = -1.011;
+    //const pdj_c = 2.08;
+    //const pdj_d = 10.2;
+    return .{
+        scale * (math.sin(pdj_a * v[1]) - math.cos(pdj_b * v[0])),
+        scale * (math.sin(pdj_c * v[0]) - math.cos(pdj_d * v[1])),
+    };
+}
+
+fn julia(v: Vec2, scale: f32, rand01: f32) Vec2 {
+    const r = scale * @sqrt(@sqrt(v[0] * v[0] + v[1] * v[1]));
+    const theta = 0.5 * math.atan2(f32, v[0], v[1]) +
+        math.pi * @intToFloat(f32, @floatToInt(i32, 2.0 * rand01));
+    const xx = r * math.cos(theta);
+    const yy = r * math.sin(theta);
+    return .{ xx, yy };
 }
