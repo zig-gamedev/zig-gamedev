c19cc52e00c4fe5b463eaaa7621957392d08b060
foxnne <foxnne@gmail.com>
2023-05-31 13:16:11 -0500

    zgui: Remove `usingnamespace` line

--- libs/zgui/src/gui.zig
+++ libs/zgui/src/gui.zig
@@ -6,7 +6,6 @@
 //--------------------------------------------------------------------------------------------------
 pub const version = @import("std").SemanticVersion{ .major = 0, .minor = 9, .patch = 6 };
 
-pub usingnamespace @import("gui.zig");
 pub const plot = @import("plot.zig");
 pub const backend = switch (@import("zgui_options").backend) {
     .glfw_wgpu => @import("backend_glfw_wgpu.zig"),
