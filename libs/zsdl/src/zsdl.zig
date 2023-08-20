9fb945c4683b67c28845919e0c5b00d2c0940641
Chris Heyes <chris@hazeycode.uk>
2023-08-20 20:45:40 +0100

    zsdl: Add basic compile test & fixes

--- libs/zsdl/src/zsdl.zig
+++ libs/zsdl/src/zsdl.zig
@@ -1,5 +1,19 @@
 const options = @import("zsdl_options");
 
-pub usingnamespace @import("sdl2.zig");
+const sdl2 = @import("sdl2.zig");
+pub usingnamespace sdl2;
 
 pub const ttf = if (options.enable_ttf) @import("ttf.zig") else @compileError("zsdl: SDL_ttf not enabled; check your build options.");
+
+test {
+    const testing = @import("std").testing;
+
+    testing.refAllDeclsRecursive(sdl2);
+
+    // TEMPORARY: uncomment to test SDL3 bindings are compilable
+    //testing.refAllDeclsRecursive(@import("sdl3.zig"));
+
+    if (options.enable_ttf) {
+        testing.refAllDeclsRecursive(ttf);
+    }
+}
