ba9fa5510fc7465cb824efa0b91a1b62356c4c93
Chris Heyes <chris@hazeycode.uk>
2023-08-21 22:35:44 +0100

    zsdl: delete temp code

--- libs/zsdl/src/zsdl.zig
+++ libs/zsdl/src/zsdl.zig
@@ -6,16 +6,14 @@ pub usingnamespace sdl2;
 pub const ttf = if (options.enable_ttf) @import("ttf.zig") else @compileError("zsdl: SDL_ttf not enabled; check your build options.");
 
 test {
-    if (false) {
-        const testing = @import("std").testing;
+    const testing = @import("std").testing;
 
-        testing.refAllDeclsRecursive(sdl2);
+    testing.refAllDeclsRecursive(sdl2);
 
-        // TEMPORARY: uncomment to test SDL3 bindings are compilable
-        //testing.refAllDeclsRecursive(@import("sdl3.zig"));
+    // TEMPORARY: uncomment to test SDL3 bindings are compilable
+    //testing.refAllDeclsRecursive(@import("sdl3.zig"));
 
-        if (options.enable_ttf) {
-            testing.refAllDeclsRecursive(ttf);
-        }
+    if (options.enable_ttf) {
+        testing.refAllDeclsRecursive(ttf);
     }
 }
