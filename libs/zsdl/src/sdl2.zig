071d3079161c21e2fb8b101df73b482d7b45dca8
Stephen Baldwin <github@bnbn.sh>
2023-08-25 16:00:08 -0700

    Update pushEvent return to Error!bool

--- libs/zsdl/src/sdl2.zig
+++ libs/zsdl/src/sdl2.zig
@@ -1251,9 +1251,14 @@ pub fn pollEvent(event: ?*Event) bool {
 }
 extern fn SDL_PollEvent(event: ?*Event) i32;
 
-/// pub fn pushEvent(event: ?*Event) i32 {
-pub const pushEvent = SDL_PushEvent;
-extern fn SDL_PushEvent(event: ?*Event) i32;
+/// Returns true if event was added
+///         false if event was filtered out
+pub fn pushEvent(event: *Event) Error!bool {
+    const status = SDL_PushEvent(event);
+    if (status < 0) return makeError();
+    return status == 1;
+}
+extern fn SDL_PushEvent(event: *Event) i32;
 
 //--------------------------------------------------------------------------------------------------
 //
