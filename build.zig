949a0534e49a674cdbe17699ae386b6246b97454
Chris Heyes <chris@hazeycode.uk>
2023-09-22 00:14:33 +0100

    Add zopengl tests

--- build.zig
+++ build.zig
@@ -228,6 +228,7 @@ fn tests(b: *std.Build, options: Options) void {
     test_step.dependOn(zaudio.runTests(b, options.optimize, options.target));
     test_step.dependOn(zflecs.runTests(b, options.optimize, options.target));
     test_step.dependOn(zphysics.runTests(b, options.optimize, options.target));
+    test_step.dependOn(zopengl.runTests(b, options.optimize, options.target));
 
     // TODO: zsdl test not included in top-level tests until https://github.com/michal-z/zig-gamedev/issues/312 is resolved
     //test_step.dependOn(zsdl.runTests(b, options.optimize, options.target));
