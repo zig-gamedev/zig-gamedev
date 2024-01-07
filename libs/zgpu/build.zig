80eb87e27b010f151df8cc9c1cd3d97cb3daf2b0
mochalins <117967760+mochalins@users.noreply.github.com>
2024-01-07 12:53:57 +0900

    scm: Update `zgpu/build.zig` for latest Zig

--- libs/zgpu/build.zig
+++ libs/zgpu/build.zig
@@ -33,68 +33,76 @@ pub const Package = struct {
     zgpu: *std.Build.Module,
     zgpu_options: *std.Build.Module,
 
-    pub fn link(pkg: Package, exe: *std.Build.CompileStep) void {
-        const target = (std.zig.system.NativeTargetInfo.detect(exe.target) catch unreachable).target;
+    pub fn link(pkg: Package, exe: *std.Build.Step.Compile) void {
+        const target = exe.rootModuleTarget();
 
-        exe.addModule("zgpu", pkg.zgpu);
-        exe.addModule("zgpu_options", pkg.zgpu_options);
+        exe.root_module.addImport("zgpu", pkg.zgpu);
+        exe.root_module.addImport("zgpu_options", pkg.zgpu_options);
 
         const b = exe.step.owner;
 
         switch (target.os.tag) {
             .windows => {
                 const dawn_dep = b.dependency("dawn_x86_64_windows_gnu", .{});
-                exe.addLibraryPath(.{ .path = dawn_dep.builder.build_root.path.? });
-                exe.addLibraryPath(.{ .path = thisDir() ++ "/../system-sdk/windows/lib/x86_64-windows-gnu" });
+                exe.root_module.addLibraryPath(.{ .path = dawn_dep.builder.build_root.path.? });
+                exe.root_module.addLibraryPath(.{
+                    .path = thisDir() ++ "/../system-sdk/windows/lib/x86_64-windows-gnu",
+                });
 
-                exe.linkSystemLibraryName("ole32");
-                exe.linkSystemLibraryName("dxguid");
+                exe.root_module.linkSystemLibrary("ole32", .{});
+                exe.root_module.linkSystemLibrary("dxguid", .{});
             },
             .linux => {
                 if (target.cpu.arch.isX86()) {
                     const dawn_dep = b.dependency("dawn_x86_64_linux_gnu", .{});
-                    exe.addLibraryPath(.{ .path = dawn_dep.builder.build_root.path.? });
+                    exe.root_module.addLibraryPath(.{ .path = dawn_dep.builder.build_root.path.? });
                 } else {
                     const dawn_dep = b.dependency("dawn_aarch64_linux_gnu", .{});
-                    exe.addLibraryPath(.{ .path = dawn_dep.builder.build_root.path.? });
+                    exe.root_module.addLibraryPath(.{ .path = dawn_dep.builder.build_root.path.? });
                 }
             },
             .macos => {
-                exe.addFrameworkPath(.{ .path = thisDir() ++ "/../system-sdk/macos12/System/Library/Frameworks" });
-                exe.addSystemIncludePath(.{ .path = thisDir() ++ "/../system-sdk/macos12/usr/include" });
-                exe.addLibraryPath(.{ .path = thisDir() ++ "/../system-sdk/macos12/usr/lib" });
+                exe.root_module.addFrameworkPath(.{
+                    .path = thisDir() ++ "/../system-sdk/macos12/System/Library/Frameworks",
+                });
+                exe.root_module.addSystemIncludePath(.{
+                    .path = thisDir() ++ "/../system-sdk/macos12/usr/include",
+                });
+                exe.root_module.addLibraryPath(.{
+                    .path = thisDir() ++ "/../system-sdk/macos12/usr/lib",
+                });
 
                 if (target.cpu.arch.isX86()) {
                     const dawn_dep = b.dependency("dawn_x86_64_macos", .{});
-                    exe.addLibraryPath(.{ .path = dawn_dep.builder.build_root.path.? });
+                    exe.root_module.addLibraryPath(.{ .path = dawn_dep.builder.build_root.path.? });
                 } else {
                     const dawn_dep = b.dependency("dawn_aarch64_macos", .{});
-                    exe.addLibraryPath(.{ .path = dawn_dep.builder.build_root.path.? });
+                    exe.root_module.addLibraryPath(.{ .path = dawn_dep.builder.build_root.path.? });
                 }
 
-                exe.linkSystemLibraryName("objc");
-                exe.linkFramework("Metal");
-                exe.linkFramework("CoreGraphics");
-                exe.linkFramework("Foundation");
-                exe.linkFramework("IOKit");
-                exe.linkFramework("IOSurface");
-                exe.linkFramework("QuartzCore");
+                exe.root_module.linkSystemLibrary("objc", .{});
+                exe.root_module.linkFramework("Metal", .{});
+                exe.root_module.linkFramework("CoreGraphics", .{});
+                exe.root_module.linkFramework("Foundation", .{});
+                exe.root_module.linkFramework("IOKit", .{});
+                exe.root_module.linkFramework("IOSurface", .{});
+                exe.root_module.linkFramework("QuartzCore", .{});
             },
             else => unreachable,
         }
 
-        exe.linkSystemLibraryName("dawn");
-        exe.linkLibC();
-        exe.linkLibCpp();
+        exe.root_module.linkSystemLibrary("dawn", .{});
+        exe.root_module.link_libc = true;
+        exe.root_module.link_libcpp = true;
 
-        exe.addIncludePath(.{ .path = thisDir() ++ "/libs/dawn/include" });
-        exe.addIncludePath(.{ .path = thisDir() ++ "/src" });
+        exe.root_module.addIncludePath(.{ .path = thisDir() ++ "/libs/dawn/include" });
+        exe.root_module.addIncludePath(.{ .path = thisDir() ++ "/src" });
 
-        exe.addCSourceFile(.{
+        exe.root_module.addCSourceFile(.{
             .file = .{ .path = thisDir() ++ "/src/dawn.cpp" },
             .flags = &.{ "-std=c++17", "-fno-sanitize=undefined" },
         });
-        exe.addCSourceFile(.{
+        exe.root_module.addCSourceFile(.{
             .file = .{ .path = thisDir() ++ "/src/dawn_proc.c" },
             .flags = &.{"-fno-sanitize=undefined"},
         });
@@ -103,7 +111,7 @@ pub const Package = struct {
 
 pub fn package(
     b: *std.Build,
-    _: std.zig.CrossTarget,
+    _: std.Build.ResolvedTarget,
     _: std.builtin.Mode,
     args: struct {
         options: Options = .{},
@@ -129,8 +137,8 @@ pub fn package(
     const zgpu_options = step.createModule();
 
     const zgpu = b.addModule("zgpu", .{
-        .source_file = .{ .path = thisDir() ++ "/src/zgpu.zig" },
-        .dependencies = &.{
+        .root_source_file = .{ .path = thisDir() ++ "/src/zgpu.zig" },
+        .imports = &.{
             .{ .name = "zgpu_options", .module = zgpu_options },
             .{ .name = "zglfw", .module = args.deps.zglfw },
             .{ .name = "zpool", .module = args.deps.zpool },
