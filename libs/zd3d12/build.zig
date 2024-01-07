0134a053e881252d12c8de04413f5e69ae747174
mochalins <117967760+mochalins@users.noreply.github.com>
2024-01-07 12:56:28 +0900

    scm: Update `zd3d12/build.zig` for latest Zig

--- libs/zd3d12/build.zig
+++ libs/zd3d12/build.zig
@@ -19,15 +19,15 @@ pub const Package = struct {
     zd3d12: *std.Build.Module,
     zd3d12_options: *std.Build.Module,
 
-    pub fn link(pkg: Package, exe: *std.Build.CompileStep) void {
-        exe.addModule("zd3d12", pkg.zd3d12);
-        exe.addModule("zd3d12_options", pkg.zd3d12_options);
+    pub fn link(pkg: Package, exe: *std.Build.Step.Compile) void {
+        exe.root_module.addImport("zd3d12", pkg.zd3d12);
+        exe.root_module.addImport("zd3d12_options", pkg.zd3d12_options);
     }
 };
 
 pub fn package(
     b: *std.Build,
-    _: std.zig.CrossTarget,
+    _: std.Build.ResolvedTarget,
     _: std.builtin.Mode,
     args: struct {
         options: Options = .{},
@@ -43,8 +43,8 @@ pub fn package(
     const zd3d12_options = step.createModule();
 
     const zd3d12 = b.addModule("zd3d12", .{
-        .source_file = .{ .path = thisDir() ++ "/src/zd3d12.zig" },
-        .dependencies = &.{
+        .root_source_file = .{ .path = thisDir() ++ "/src/zd3d12.zig" },
+        .imports = &.{
             .{ .name = "zd3d12_options", .module = zd3d12_options },
             .{ .name = "zwin32", .module = args.deps.zwin32 },
         },
