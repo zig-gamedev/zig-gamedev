9fe306d48ed7dc7ef3930ff4713d6bc04a04a882
Chris Heyes <22148308+hazeycode@users.noreply.github.com>
2024-01-11 21:38:02 +0000

    zgpu: expose package path

--- libs/zgpu/build.zig
+++ libs/zgpu/build.zig
@@ -1,8 +1,14 @@
 const std = @import("std");
-const system_sdk = @import("system_sdk");
 
+const system_sdk = @import("system_sdk");
 const zglfw = @import("zglfw");
 const zpool = @import("zpool");
+
+pub const path = getPath();
+
+inline fn getPath() []const u8 {
+    return std.fs.path.dirname(@src().file) orelse unreachable;
+}
 
 const default_options = struct {
     const uniforms_buffer_size = 4 * 1024 * 1024;
@@ -97,15 +103,15 @@ pub const Package = struct {
         exe.linkLibC();
         exe.linkLibCpp();
 
-        exe.addIncludePath(.{ .path = thisDir() ++ "/libs/dawn/include" });
-        exe.addIncludePath(.{ .path = thisDir() ++ "/src" });
+        exe.addIncludePath(.{ .path = path ++ "/libs/dawn/include" });
+        exe.addIncludePath(.{ .path = path ++ "/src" });
 
         exe.addCSourceFile(.{
-            .file = .{ .path = thisDir() ++ "/src/dawn.cpp" },
+            .file = .{ .path = path ++ "/src/dawn.cpp" },
             .flags = &.{ "-std=c++17", "-fno-sanitize=undefined" },
         });
         exe.addCSourceFile(.{
-            .file = .{ .path = thisDir() ++ "/src/dawn_proc.c" },
+            .file = .{ .path = path ++ "/src/dawn_proc.c" },
             .flags = &.{"-fno-sanitize=undefined"},
         });
     }
@@ -113,7 +119,7 @@ pub const Package = struct {
     pub fn makeTestStep(pkg: Package, b: *std.Build) *std.Build.Step {
         const tests = b.addTest(.{
             .name = "zgpu-tests",
-            .root_source_file = .{ .path = thisDir() ++ "/src/zgpu.zig" },
+            .root_source_file = .{ .path = path ++ "/src/zgpu.zig" },
             .target = pkg.target,
             .optimize = pkg.optimize,
         });
@@ -152,7 +158,7 @@ pub fn package(
     const zgpu_options = step.createModule();
 
     const zgpu = b.addModule("zgpu", .{
-        .source_file = .{ .path = thisDir() ++ "/src/zgpu.zig" },
+        .source_file = .{ .path = path ++ "/src/zgpu.zig" },
         .dependencies = &.{
             .{ .name = "zgpu_options", .module = zgpu_options },
             .{ .name = "zglfw", .module = args.deps.zglfw.zglfw },
@@ -246,8 +252,4 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run zgpu tests");
     test_step.dependOn(pkg.makeTestStep(b));
-}
-
-inline fn thisDir() []const u8 {
-    return comptime std.fs.path.dirname(@src().file) orelse ".";
 }
