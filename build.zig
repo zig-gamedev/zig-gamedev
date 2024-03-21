b911bfe477c194a3c2295fff960114694eab78a3
Chris Heyes <22148308+hazeycode@users.noreply.github.com>
2024-03-21 00:02:06 +0000

    Up Zig to 0.12.0-dev.3180+83e578a18

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const builtin = @import("builtin");
 const std = @import("std");
 
-pub const min_zig_version = std.SemanticVersion{ .major = 0, .minor = 12, .patch = 0, .pre = "dev.2063" };
+pub const min_zig_version = std.SemanticVersion{ .major = 0, .minor = 12, .patch = 0, .pre = "dev.3180" };
 
 pub fn build(b: *std.Build) !void {
     ensureZigVersion() catch return;
