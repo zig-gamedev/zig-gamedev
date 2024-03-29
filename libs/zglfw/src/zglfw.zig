747096d5a769d2e46b835c5b44ceaac82976e93b
hazeycode <22148308+hazeycode@users.noreply.github.com>
2024-03-29 12:19:59 +0000

    zig fmt

--- libs/zglfw/src/zglfw.zig
+++ libs/zglfw/src/zglfw.zig
@@ -816,9 +816,8 @@ pub const Window = opaque {
     pub const swapBuffers = glfwSwapBuffers;
     extern fn glfwSwapBuffers(window: *Window) void;
 
-    pub fn setMonitor (window: *Window, monitor: ?*Monitor, xpos: i32, ypos: i32, width: i32, height: i32, refreshRate: i32) void
-    {
-        glfwSetWindowMonitor (window, monitor, xpos, ypos, width, height, refreshRate);
+    pub fn setMonitor(window: *Window, monitor: ?*Monitor, xpos: i32, ypos: i32, width: i32, height: i32, refreshRate: i32) void {
+        glfwSetWindowMonitor(window, monitor, xpos, ypos, width, height, refreshRate);
     }
     extern fn glfwSetWindowMonitor(window: *Window, monitor: ?*Monitor, xpos: i32, ypos: i32, width: i32, height: i32, refreshRate: i32) void;
 
