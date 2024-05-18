722c8694a5d5058c8755f1fa1b587fd4ebcbc12d
Ronald Chen <pyrogx1133@gmail.com>
2024-05-17 20:29:14 -0700

    gamepad_wgpu: removed thisDir()

--- samples/gamepad_wgpu/build.zig
+++ samples/gamepad_wgpu/build.zig
@@ -6,9 +6,11 @@ const demo_name = "gamepad_wgpu";
 const content_dir = demo_name ++ "_content/";
 
 pub fn build(b: *std.Build, options: Options) *std.Build.Step.Compile {
+    const cwd_path = b.pathJoin(&.{ "samples", demo_name });
+    const src_path = b.pathJoin(&.{ cwd_path, "src" });
     const exe = b.addExecutable(.{
         .name = demo_name,
-        .root_source_file = .{ .path = thisDir() ++ "/src/" ++ demo_name ++ ".zig" },
+        .root_source_file = b.path(b.pathJoin(&.{ src_path, demo_name ++ ".zig" })),
         .target = options.target,
         .optimize = options.optimize,
     });
@@ -44,16 +46,13 @@ pub fn build(b: *std.Build, options: Options) *std.Build.Step.Compile {
     exe.root_module.addOptions("build_options", exe_options);
     exe_options.addOption([]const u8, "content_dir", content_dir);
 
+    const content_path = b.pathJoin(&.{ cwd_path, content_dir });
     const install_content_step = b.addInstallDirectory(.{
-        .source_dir = .{ .path = thisDir() ++ "/" ++ content_dir },
+        .source_dir = b.path(content_path),
         .install_dir = .{ .custom = "" },
-        .install_subdir = "bin/" ++ content_dir,
+        .install_subdir = b.pathJoin(&.{ "bin", content_dir }),
     });
     exe.step.dependOn(&install_content_step.step);
 
     return exe;
-}
-
-inline fn thisDir() []const u8 {
-    return comptime std.fs.path.dirname(@src().file) orelse ".";
 }
