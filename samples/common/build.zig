abbec36d1e0fb2d644f4e69a4883857d312e0ce2
Ronald Chen <pyrogx1133@gmail.com>
2024-05-17 21:46:19 -0700

    common: removed LazyPath.path usage

--- samples/common/build.zig
+++ samples/common/build.zig
@@ -19,44 +19,44 @@ pub fn link(compile_step: *std.Build.Step.Compile, deps: struct {
         lib.linkLibCpp();
     lib.linkSystemLibrary("imm32");
 
-    lib.addIncludePath(.{ .path = "libs" });
+    lib.addIncludePath(b.path("libs"));
     lib.addCSourceFile(
-        .{ .file = .{ .path = "samples/common/libs/imgui/imgui.cpp" }, .flags = &.{""} },
+        .{ .file = b.path("samples/common/libs/imgui/imgui.cpp"), .flags = &.{""} },
     );
     lib.addCSourceFile(
-        .{ .file = .{ .path = "samples/common/libs/imgui/imgui_widgets.cpp" }, .flags = &.{""} },
+        .{ .file = b.path("samples/common/libs/imgui/imgui_widgets.cpp"), .flags = &.{""} },
     );
     lib.addCSourceFile(
-        .{ .file = .{ .path = "samples/common/libs/imgui/imgui_tables.cpp" }, .flags = &.{""} },
+        .{ .file = b.path("samples/common/libs/imgui/imgui_tables.cpp"), .flags = &.{""} },
     );
     lib.addCSourceFile(
-        .{ .file = .{ .path = "samples/common/libs/imgui/imgui_draw.cpp" }, .flags = &.{""} },
+        .{ .file = b.path("samples/common/libs/imgui/imgui_draw.cpp"), .flags = &.{""} },
     );
     lib.addCSourceFile(
-        .{ .file = .{ .path = "samples/common/libs/imgui/imgui_demo.cpp" }, .flags = &.{""} },
+        .{ .file = b.path("samples/common/libs/imgui/imgui_demo.cpp"), .flags = &.{""} },
     );
     lib.addCSourceFile(
-        .{ .file = .{ .path = "samples/common/libs/imgui/cimgui.cpp" }, .flags = &.{""} },
+        .{ .file = b.path("samples/common/libs/imgui/cimgui.cpp"), .flags = &.{""} },
     );
 
-    lib.addIncludePath(.{ .path = "libs/zmesh/libs/cgltf" });
+    lib.addIncludePath(b.path("libs/zmesh/libs/cgltf"));
     lib.addCSourceFile(.{
-        .file = .{ .path = "libs/zmesh/libs/cgltf/cgltf.c" },
+        .file = b.path("libs/zmesh/libs/cgltf/cgltf.c"),
         .flags = &.{"-std=c99"},
     });
 
-    lib.addIncludePath(.{ .path = "samples/common/libs" });
-    lib.addIncludePath(.{ .path = "libs/zmesh/libs/cgltf" });
+    lib.addIncludePath(b.path("samples/common/libs"));
+    lib.addIncludePath(b.path("libs/zmesh/libs/cgltf"));
 
     const module = b.createModule(.{
-        .root_source_file = .{ .path = "samples/common/src/common.zig" },
+        .root_source_file = b.path("samples/common/src/common.zig"),
         .imports = &.{
             .{ .name = "zwin32", .module = deps.zwin32 },
             .{ .name = "zd3d12", .module = deps.zd3d12 },
         },
     });
-    module.addIncludePath(.{ .path = "samples/common/libs/imgui" });
-    module.addIncludePath(.{ .path = "libs/zmesh/libs/cgltf" });
+    module.addIncludePath(b.path("samples/common/libs/imgui"));
+    module.addIncludePath(b.path("libs/zmesh/libs/cgltf"));
 
     compile_step.root_module.addImport("common", module);
 
