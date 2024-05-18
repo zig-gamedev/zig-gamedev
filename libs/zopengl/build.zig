3707892bbfc80d35b8cfac17d684155035fd5e5f
Ronald Chen <pyrogx1133@gmail.com>
2024-05-17 21:07:25 -0700

    zopengl: removed LazyPath.path usage

--- libs/zopengl/build.zig
+++ libs/zopengl/build.zig
@@ -5,14 +5,14 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
     _ = b.addModule("root", .{
-        .root_source_file = .{ .path = "src/zopengl.zig" },
+        .root_source_file = b.path("src/zopengl.zig"),
     });
 
     const test_step = b.step("test", "Run zopengl tests");
 
     const tests = b.addTest(.{
         .name = "zopengl-tests",
-        .root_source_file = .{ .path = "src/zopengl.zig" },
+        .root_source_file = b.path("src/zopengl.zig"),
         .target = target,
         .optimize = optimize,
     });
