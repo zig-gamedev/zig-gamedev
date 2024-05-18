1b2474523a4eef3760243618e3e55647ff2cccb1
Ronald Chen <pyrogx1133@gmail.com>
2024-05-17 21:03:16 -0700

    zjobs: removed LazyPath.path usage

--- libs/zjobs/build.zig
+++ libs/zjobs/build.zig
@@ -5,14 +5,14 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
     _ = b.addModule("root", .{
-        .root_source_file = .{ .path = "src/zjobs.zig" },
+        .root_source_file = b.path("src/zjobs.zig"),
     });
 
     const test_step = b.step("test", "Run zjobs tests");
 
     const tests = b.addTest(.{
         .name = "zjobs-tests",
-        .root_source_file = .{ .path = "src/zjobs.zig" },
+        .root_source_file = b.path("src/zjobs.zig"),
         .target = target,
         .optimize = optimize,
     });
