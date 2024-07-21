08348c8d350b2b519d39814399ff2740bdb631fc
hazeycode <22148308+hazeycode@users.noreply.github.com>
2024-07-21 23:49:05 +0100

    zsdl build.zig tidyup

--- samples/minimal_sdl_gl/build.zig
+++ samples/minimal_sdl_gl/build.zig
@@ -12,15 +12,19 @@ pub fn build(b: *std.Build, options: anytype) *std.Build.Step.Compile {
         .optimize = options.optimize,
     });
 
-    const zsdl = b.dependency("zsdl", .{
-        .target = options.target,
-    });
+    const zsdl = b.dependency("zsdl", .{});
     exe.root_module.addImport("zsdl2", zsdl.module("zsdl2"));
 
-    @import("zsdl").addLibraryPathsTo(exe);
     @import("zsdl").link_SDL2(exe);
 
-    @import("zsdl").install_sdl2(&exe.step, options.target.result, .bin);
+    const sdl2_libs_path = b.dependency("sdl2-prebuilt", .{}).path("").getPath(b);
+
+    @import("zsdl").addLibraryPathsTo(sdl2_libs_path, exe);
+    @import("zsdl").addRPathsTo(sdl2_libs_path, exe);
+
+    if (@import("zsdl").install_SDL2(b, options.target.result, sdl2_libs_path, .bin)) |install_sdl2_step| {
+        b.getInstallStep().dependOn(install_sdl2_step);
+    }
 
     const zopengl = b.dependency("zopengl", .{});
     exe.root_module.addImport("zopengl", zopengl.module("root"));
