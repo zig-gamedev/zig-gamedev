53b2566588a3adacb146618bbe3f3bd6156f7bad
DDosha <me@dosha.dev>
2024-07-26 02:11:34 +0300

    add build as shared option option (#647)
    * add build as shared option option
    
    * Update build.zig

--- libs/zflecs/build.zig
+++ libs/zflecs/build.zig
@@ -1,14 +1,18 @@
 const std = @import("std");
-
+const builtin = @import("builtin");
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
-
+    const opt_use_shared = b.option(bool, "shared", "Make shared (default: false)") orelse false;
     _ = b.addModule("root", .{
         .root_source_file = b.path("src/zflecs.zig"),
     });
 
-    const flecs = b.addStaticLibrary(.{
+    const flecs = if (opt_use_shared) b.addSharedLibrary(.{
+        .name = "flecs",
+        .target = target,
+        .optimize = optimize,
+    }) else b.addStaticLibrary(.{
         .name = "flecs",
         .target = target,
         .optimize = optimize,
@@ -21,7 +25,8 @@ pub fn build(b: *std.Build) void {
             "-fno-sanitize=undefined",
             "-DFLECS_NO_CPP",
             "-DFLECS_USE_OS_ALLOC",
-            if (@import("builtin").mode == .Debug) "-DFLECS_SANITIZE" else "",
+            if (builtin.mode == .Debug) "-DFLECS_SANITIZE" else "",
+            if (opt_use_shared) "-DFLECS_SHARED" else "",
         },
     });
     b.installArtifact(flecs);
@@ -29,7 +34,6 @@ pub fn build(b: *std.Build) void {
     if (target.result.os.tag == .windows) {
         flecs.linkSystemLibrary("ws2_32");
     }
-
     const test_step = b.step("test", "Run zflecs tests");
 
     const tests = b.addTest(.{
