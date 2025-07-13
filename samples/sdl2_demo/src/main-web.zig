b4f58d1f4fd727918c31deee4461db6e0d6b8cd1
Chris Heyes <22148308+hazeycode@users.noreply.github.com>
2025-07-13 02:42:29 +0100

    Simplify sdl2_demo/src/main_web.zig

--- samples/sdl2_demo/src/main-web.zig
+++ samples/sdl2_demo/src/main-web.zig
@@ -9,21 +9,18 @@ pub const std_options = std.Options{
 
 const sdl2_demo = @import("sdl2_demo.zig");
 
-var initialised = false;
-
 export fn main() c_int {
+    sdl2_demo.init() catch |err| {
+        std.log.err("sdl_demo.init failed with error: {s}", .{@errorName(err)});
+        return -1;
+    };
+
     zemscripten.setMainLoop(mainLoopCallback, null, false);
+
     return 0;
 }
 
 export fn mainLoopCallback() void {
-    if (initialised == false) {
-        sdl2_demo.init() catch |err| {
-            std.log.err("sdl_demo.init failed with error: {s}", .{@errorName(err)});
-            return;
-        };
-        initialised = true;
-    }
     sdl2_demo.updateAndRender() catch |err| {
         std.log.err("sdl_demo.updateAndRender failed with error: {s}", .{@errorName(err)});
     };
